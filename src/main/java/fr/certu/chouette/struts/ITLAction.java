174646ec5682af7a1b9425ded35f05f69fca87cb
luc <luc@alpha.(none)>
2009-11-25 15:14:01 +0100

    Mise a jour des ITL

--- src/main/java/fr/certu/chouette/struts/ITLAction.java
+++ src/main/java/fr/certu/chouette/struts/ITLAction.java
@@ -2,11 +2,9 @@ package fr.certu.chouette.struts;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.struts2.interceptor.RequestAware;
 import org.apache.struts2.interceptor.validation.SkipValidation;
 
 import com.opensymphony.xwork2.Preparable;
@@ -18,340 +16,368 @@ import fr.certu.chouette.service.database.IITLManager;
 import fr.certu.chouette.service.database.ILigneManager;
 import fr.certu.chouette.service.database.IPositionGeographiqueManager;
 
-public class ITLAction extends GeneriqueAction implements RequestAware, Preparable
+public class ITLAction extends GeneriqueAction implements Preparable
 {
-	private Map request;
-	private final Log						log	= LogFactory.getLog(ITLAction.class);
-	private static IITLManager				itlManager;
-	private static ILigneManager			ligneManager;
-	private static IPositionGeographiqueManager	positionGeographiqueManager;
-	private List<InterdictionTraficLocal>	itls;
-	private InterdictionTraficLocal			itl;
-	private Long							idItl;
-	private List<Ligne>						lignes;
-	private List<PositionGeographique>				arrets;
-	private String 							ligneName;			
-	
-	private List<PositionGeographique> arretsDansITLList = new ArrayList();
 
-	private String saisieNomArretExistant;
-	private String saisieNomArretExistantKey;
-	private String idAreaStop;
-	
-	private Long						idLigne;
-	
-	@SkipValidation
-	public String cancel()
-	{
-		addActionMessage(getText("itl.cancel.ok"));
-		return SUCCESS;
-	}
-	
-	@SkipValidation
-	public String delete()
-	{
-		itlManager.supprimer(idItl);
-		return SUCCESS;
-	}
-	
-	public List<InterdictionTraficLocal> getItls()
-	{
-		return itls;
-	}
+  private final Log log = LogFactory.getLog(ITLAction.class);
+  private static IITLManager itlManager;
+  private static ILigneManager ligneManager;
+  private static IPositionGeographiqueManager positionGeographiqueManager;
+  private InterdictionTraficLocal itl;
+  private Long idItl;
+  private Long idAreaStop;
+  private List<PositionGeographique> arrets;
+  private List<PositionGeographique> arretsDansITLList;
+  private String saisieNomArretExistant;
+  private String saisieNomArretExistantKey;
+  private String mappedRequest;
+  private String name;
 
-	public void setIdItl(Long idItl)
-	{
-		this.idItl = idItl;
-	}
+  public Long getIdItl()
+  {
+    return idItl;
+  }
 
-	public void setManager(IITLManager manager)
-	{
-		this.itlManager = manager;
-	}
-	
-	public void setLigneManager(ILigneManager ligneManager)
-	{
-		this.ligneManager = ligneManager;
-	}
-	
-	@SkipValidation
-	public String list()
-	{
-		itls = itlManager.lire();
-		return SUCCESS;
-	}
-	
-	@SkipValidation
-	public String edit()
-	{	
-		return INPUT;
-	}
-	
-	@SkipValidation
-	public String addStop()
-	{
-		if (saisieNomArretExistantKey != null && !saisieNomArretExistantKey.isEmpty())
-		{
-			List<Long> l = this.itl.getArretPhysiqueIds();
-			l.add(Long.valueOf(saisieNomArretExistantKey));
-			itl.setArretPhysiqueIds(l);
-			readArretsDansITL();
-			try {
-				itlManager.modifier(itl);
-				addActionMessage(getText("itl.update.ok"));
-			} 
-			catch (Exception ex) { 
-				addActionError(getText("itl.update.ko"));
-			}
-		}
-		return INPUT;
-	}	
-	
-	@SkipValidation
-	public String removeStop()
-	{
-		if (idAreaStop != null && !idAreaStop.isEmpty())
-		{
-			List<Long> l = this.itl.getArretPhysiqueIds();
-			int idx = l.indexOf(Long.valueOf(idAreaStop));
-			if (idx != -1) {
-				l.remove(idx);
-				try {
-					itlManager.modifier(itl);
-					addActionMessage(getText("itl.update.ok"));
-				} 
-				catch (Exception ex) { 
-					addActionError(getText("itl.update.ko"));
-				}
-			}
-			readArretsDansITL();
-		}
-		return INPUT;
-	}	
-	
+  public void setIdItl(Long idItl)
+  {
+    this.idItl = idItl;
+  }
 
-	
-	@SkipValidation
-	public void prepare() throws Exception
-	{				
-		//readLignes();
-		
-		if (idItl == null) {
-			//if (itl != null && itl.getIdLigne() != null) {				
-			//	request.put("jsonArrets", getJsonArrets(itl.getIdLigne()));
-			//}
-			return;
-		}
-		itl = itlManager.lire(idItl);
-		readArretsDansITL();
-		//if (lignes != null)
-		//	lignes = ligneManager.lire();
-		//request.put("jsonArrets", getJsonArrets(itl.getIdLigne()));
-		
-		
-	}
+  public String getName()
+  {
+    return name;
+  }
 
-	public String getJsonArrets(Long idLigne)
-	{
-		String resultat = "";
-		
-		List<PositionGeographique> arretsPhysiques = null;
-		if (idLigne == null)
-				arretsPhysiques = positionGeographiqueManager.lireArretsPhysiques();
-		else {
-			//arretsPhysiques = arretPhysiqueManager.getArretsPhysiques(idLigne);
-			arretsPhysiques = positionGeographiqueManager.lireArretsPhysiques();
-		}
-		
-		resultat += "{";
-		for (PositionGeographique arretPhysique : arretsPhysiques)
-		{
-			if (arretsPhysiques.indexOf(arretPhysique) == arretsPhysiques
-					.size() - 1)
-			{
-				resultat += "\"" + arretPhysique.getName() + "\"" + ": "
-						+ arretPhysique.getId();
-			} else
-			{
-				resultat += "\"" + arretPhysique.getName() + "\"" + ": "
-						+ arretPhysique.getId() + ",";
-			}
-		}
-		resultat += "}";
-		return resultat;
-	}
-	
-	public String update()
-	{					
-		log.debug("update");
-		if (itl == null) { return INPUT; }
-						
-		if ( itl.getId()==null)
-		{
-			try {
-				itlManager.creer(itl);				
-				addActionMessage(getText("itl.create.ok"));
-			} 
-			catch (Exception ex) { 
-				addActionError(getText("itl.create.ko"));
-			}
-		}
-		else
-		{
-			InterdictionTraficLocal itlOld = itlManager.lire(itl.getId());
-			if (!itlOld.getIdLigne().equals(itl.getIdLigne())) {
-				//List<ArretPhysique> l = arretPhysiqueManager.getArretsPhysiques(itl.getIdLigne());
-				List<PositionGeographique> l = positionGeographiqueManager.lireArretsPhysiques();
-				session.put("arrets", l);
-			}
-			try {
-				itlManager.modifier(itl);
-				addActionMessage(getText("itl.update.ok"));
-			} 
-			catch (Exception ex) { 
-				addActionError(getText("itl.update.ko"));
-			}
-		}			
-		return INPUT;
-	}
+  public void setName(String name)
+  {
+    this.name = name;
+  }
 
-	public InterdictionTraficLocal getItl() {
-		return itl;
-	}
+  public Long getIdAreaStop()
+  {
+    return idAreaStop;
+  }
 
-	public void setItl(InterdictionTraficLocal itl) {
-		this.itl = itl;
-	}
+  public void setIdAreaStop(Long idAreaStop)
+  {
+    this.idAreaStop = idAreaStop;
+  }
 
-	public List<Ligne> getLignes() {
-		readLignes();
-		return lignes;
-	}
-	
-	public void setRequest(Map request)
-	{
-		this.request = request;
-	}
+  public String getChaineIdLigne()
+  {
+    return itl.getIdLigne().toString();
+  }
 
-	public void setPositionGeographiqueManager(
-			IPositionGeographiqueManager positionGeographiqueManager) {
-		ITLAction.positionGeographiqueManager = positionGeographiqueManager;
-	}
+  /********************************************************
+   *                  MODEL + PREPARE                     *
+   ********************************************************/
+  @SkipValidation
+  public void prepare() throws Exception
+  {
+    log.debug("Prepare with id : " + getIdItl());
+    if (getIdItl() == null)
+    {
+      itl = new InterdictionTraficLocal();
+    }
+    else
+    {
+      itl = itlManager.lire(getIdItl());
+    }
+  }
 
-	public String getSaisieNomArretExistant() {
-		return saisieNomArretExistant;
-	}
+  public InterdictionTraficLocal getItl()
+  {
+    return itl;
+  }
 
-	public void setSaisieNomArretExistant(String saisieNomArretExistant) {
-		this.saisieNomArretExistant = saisieNomArretExistant;
-	}
+  public void setItl(InterdictionTraficLocal itl)
+  {
+    this.itl = itl;
+  }
 
-	public String getSaisieNomArretExistantKey() {
-		return saisieNomArretExistantKey;
-	}
+  /********************************************************
+   *                           CRUD                       *
+   ********************************************************/
+  @SkipValidation
+  public String cancel()
+  {
+    addActionMessage(getText("itl.cancel.ok"));
+    return REDIRECTLIST;
+  }
 
-	public void setSaisieNomArretExistantKey(String saisieNomArretExistantKey) {
-		this.saisieNomArretExistantKey = saisieNomArretExistantKey;
-	}
+  @SkipValidation
+  public String delete()
+  {
+    itlManager.supprimer(getItl().getId());
+    addActionMessage(getText("itl.delete.ok"));
+    log.debug("Delete itl with id : " + getItl().getId());
+    return REDIRECTLIST;
+  }
 
-	public String getIdAreaStop() {
-		return idAreaStop;
-	}
+  @SkipValidation
+  public String list()
+  {
+    this.request.put("itls", itlManager.lire());
+    log.debug("List of itls");
+    return LIST;
+  }
 
-	public void setIdAreaStop(String idAreaStop) {
-		this.idAreaStop = idAreaStop;
-	}
+  @SkipValidation
+  public String add()
+  {
+    setMappedRequest(SAVE);
+    return EDIT;
+  }
 
-	public Long getIdLigne() {
-		return idLigne;
-	}
+  public String save()
+  {
+    try
+    {
+      itlManager.creer(itl);
+      addActionMessage(getText("itl.create.ok"));
+    }
+    catch (Exception exception)
+    {
+      addActionError(getText("itl.create.ko"));
+    }
+    setMappedRequest(SAVE);
+    log.debug("Create itl with id : " + getItl().getId());
+    return REDIRECTEDIT;
+  }
 
-	public void setIdLigne(Long idLigne) {
-		this.idLigne = idLigne;
-	}
+  @SkipValidation
+  public String edit()
+  {
+    setMappedRequest(UPDATE);
+    return EDIT;
+  }
 
-	public String getChaineIdLigne()
-	{
-		return itl.getIdLigne().toString();
-	}
+  public String update()
+  {
+    try
+    {
+      itlManager.modifier(itl);
+      addActionMessage(getText("itl.update.ok"));
+    }
+    catch (Exception exception)
+    {
+      addActionError(getText("itl.update.ko"));
+    }
+    setMappedRequest(UPDATE);
+    log.debug("Update itl with id : " + getItl().getId());
+    return REDIRECTEDIT;
+  }
 
-	public Map getRequest() {
-		return request;
-	}
-	
-	private void readLignes() {
-		lignes = ligneManager.lire();
-		/*
-		List<Ligne> l = (List<Ligne>)this.getSession().get("lignes");
-		if (l == null) {
-			lignes = ligneManager.lire();
-			this.getSession().put("lignes", lignes);
-		}
-		else 
-			lignes = l;	
-		*/	
-	}
+  @SkipValidation
+  public String addStop()
+  {
+    if (saisieNomArretExistantKey != null && !saisieNomArretExistantKey.isEmpty())
+    {
+      List<Long> l = this.itl.getArretPhysiqueIds();
+      l.add(Long.valueOf(saisieNomArretExistantKey));
+      itl.setArretPhysiqueIds(l);
+      try
+      {
+        itlManager.modifier(itl);
+        addActionMessage(getText("itl.update.ok"));
+      }
+      catch (Exception ex)
+      {
+        addActionError(getText("itl.update.ko"));
+      }
+    }
+    return REDIRECTEDIT;
+  }
 
-	public List<PositionGeographique> getArrets() {
-		List<PositionGeographique> l = (List<PositionGeographique>)session.get("arrets");
-		//if (l == null) {
-			arrets = ligneManager.getArretsPhysiques(itl.getIdLigne());
-			
-			if (arretsDansITLList != null) {
-				for (int i=0; i<arretsDansITLList.size(); i++) {
-					if (arrets != null) {
-						for (int j=0; j<arrets.size(); j++) {
-							Long l1 = arretsDansITLList.get(i).getId();
-							Long l2 = arrets.get(j).getId();
-							if (l1.equals(l2)) {
-								arrets.remove(j);
-							}
-						}
-					}
-					arrets.remove(arretsDansITLList.get(i));
-				}
-			}
-			
-			session.put("arrets", arrets);
-		
-		return arrets;
-	}
+  @SkipValidation
+  public String removeStop()
+  {
+    if (idAreaStop != null)
+    {
+      List<Long> l = this.itl.getArretPhysiqueIds();
+      int idx = l.indexOf(Long.valueOf(idAreaStop));
+      if (idx != -1)
+      {
+        l.remove(idx);
+        try
+        {
+          itlManager.modifier(itl);
+          addActionMessage(getText("itl.update.ok"));
+        }
+        catch (Exception ex)
+        {
+          addActionError(getText("itl.update.ko"));
+        }
+      }
+    }
+    return INPUT;
+  }
 
-	public void setArrets(List<PositionGeographique> arrets) {
-		this.arrets = arrets;
-	}
+  /********************************************************
+   *                        MANAGER                       *
+   ********************************************************/
+  public void setItlManager(IITLManager itlManager)
+  {
+    ITLAction.itlManager = itlManager;
+  }
 
-	private void readArretsDansITL() {
-		arretsDansITLList = new ArrayList();
-		if (itl != null) {
-			List list_id = itl.getArretPhysiqueIds();
-			if (list_id != null) {
-				for (int i=0; i<list_id.size();i++) {
-					Long id = (Long)list_id.get(i);
-					PositionGeographique ap = positionGeographiqueManager.lire(id);
-					arretsDansITLList.add(ap);
-				}
-			}	
-		}
-	}
-	
-	public List<PositionGeographique> getArretsDansITLList() {
-		
-		return arretsDansITLList;
-	}
+  public void setLigneManager(ILigneManager ligneManager)
+  {
+    ITLAction.ligneManager = ligneManager;
+  }
 
-	public void setArretsDansITLList(List arretsDansITLList) {
-		itl.setArretPhysiqueIds(arretsDansITLList);
-	}
+  public void setPositionGeographiqueManager(
+          IPositionGeographiqueManager positionGeographiqueManager)
+  {
+    ITLAction.positionGeographiqueManager = positionGeographiqueManager;
+  }
 
-	public String getLigneName() {
-		if (itl != null && itl.getIdLigne() != null) {
-			Ligne l = ligneManager.lire(itl.getIdLigne());
-			return l.getFullName();
-		}
-		else return null;
-		
-	}
+  /********************************************************
+   *                        JSON                          *
+   ********************************************************/
+  public String getJsonArrets(Long idLigne)
+  {
+    String resultat = "";
 
+    List<PositionGeographique> arretsPhysiques = null;
+    if (idLigne == null)
+    {
+      arretsPhysiques = positionGeographiqueManager.lireArretsPhysiques();
+    }
+    else
+    {
+      //arretsPhysiques = arretPhysiqueManager.getArretsPhysiques(idLigne);
+      arretsPhysiques = positionGeographiqueManager.lireArretsPhysiques();
+    }
+
+    resultat += "{";
+    for (PositionGeographique arretPhysique : arretsPhysiques)
+    {
+      if (arretsPhysiques.indexOf(arretPhysique) == arretsPhysiques.size() - 1)
+      {
+        resultat += "\"" + arretPhysique.getName() + "\"" + ": " + arretPhysique.getId();
+      }
+      else
+      {
+        resultat += "\"" + arretPhysique.getName() + "\"" + ": " + arretPhysique.getId() + ",";
+      }
+    }
+    resultat += "}";
+    return resultat;
+  }
+
+  /********************************************************
+   *                     RESEARCH FORM                    *
+   ********************************************************/
+  public String getSaisieNomArretExistant()
+  {
+    return saisieNomArretExistant;
+  }
+
+  public void setSaisieNomArretExistant(String saisieNomArretExistant)
+  {
+    this.saisieNomArretExistant = saisieNomArretExistant;
+  }
+
+  public String getSaisieNomArretExistantKey()
+  {
+    return saisieNomArretExistantKey;
+  }
+
+  public void setSaisieNomArretExistantKey(String saisieNomArretExistantKey)
+  {
+    this.saisieNomArretExistantKey = saisieNomArretExistantKey;
+  }
+
+  public List<PositionGeographique> getArrets()
+  {
+    arrets = ligneManager.getArretsPhysiques(itl.getIdLigne());
+    
+    if (arretsDansITLList != null)
+    {
+      for (int i = 0; i < arretsDansITLList.size(); i++)
+      {
+        if (arrets != null)
+        {
+          for (int j = 0; j < arrets.size(); j++)
+          {
+            Long l1 = arretsDansITLList.get(i).getId();
+            Long l2 = arrets.get(j).getId();
+            if (l1.equals(l2))
+            {
+              arrets.remove(j);
+            }
+          }
+        }
+        arrets.remove(arretsDansITLList.get(i));
+      }
+    }
+
+    return arrets;
+  }
+
+  public List<PositionGeographique> getArretsDansITLList()
+  {
+    arretsDansITLList = new ArrayList();
+    if (itl != null)
+    {
+      List list_id = itl.getArretPhysiqueIds();
+      if (list_id != null)
+      {
+        for (int i = 0; i < list_id.size(); i++)
+        {
+          Long id = (Long) list_id.get(i);
+          PositionGeographique ap = positionGeographiqueManager.lire(id);
+          arretsDansITLList.add(ap);
+        }
+      }
+    }
+    return arretsDansITLList;
+  }
+
+  /********************************************************
+   *                     Line                             *
+   ********************************************************/
+  /**
+   * Retourne le nom de la ligne
+   *
+   * @return string
+   */
+  public String getLigneName()
+  {
+    if (itl != null && itl.getIdLigne() != null)
+    {
+      Ligne l = ligneManager.lire(itl.getIdLigne());
+      return l.getFullName();
+    }
+    else
+    {
+      return null;
+    }
+  }
+
+  public List<Ligne> getLignes()
+  {
+    return ligneManager.lire();
+  }
+
+  /********************************************************
+   *                   METHODE ACTION                     *
+   ********************************************************/
+  // this prepares command for button on initial screen write
+  public void setMappedRequest(String actionMethod)
+  {
+    this.mappedRequest = actionMethod;
+  }
+
+  // when invalid, the request parameter will restore command action
+  public void setActionMethod(String method)
+  {
+    this.mappedRequest = method;
+  }
+
+  public String getActionMethod()
+  {
+    return mappedRequest;
+  }
 }
-
