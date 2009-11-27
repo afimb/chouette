f6b85f960774e8faf5e5dcd9ab44211ed7b45e73
luc <luc@alpha.(none)>
2009-11-27 11:38:37 +0100

    Ajout des modifs

--- chouette/src/main/java/fr/certu/chouette/struts/itineraire/ItineraireAction.java
+++ chouette/src/main/java/fr/certu/chouette/struts/itineraire/ItineraireAction.java
@@ -16,78 +16,72 @@ import fr.certu.chouette.struts.GeneriqueAction;
 import fr.certu.chouette.modele.Itineraire;
 import fr.certu.chouette.service.database.IItineraireManager;
 import fr.certu.chouette.service.database.ILigneManager;
+import java.util.HashMap;
 
-public class ItineraireAction extends GeneriqueAction implements ModelDriven, Preparable
+public class ItineraireAction extends GeneriqueAction implements ModelDriven<Itineraire>, Preparable
 {
 
   private static final Log log = LogFactory.getLog(ItineraireAction.class);
-  //	Manager
-  private static IItineraireManager itineraireManager;
-  private static ILigneManager ligneManager;
-  private RouteModel itineraireModel = new RouteModel();
+  private static ILigneManager lineManager;
+  private Itineraire itineraryModel = new Itineraire();
+  private static IItineraireManager itineraryManager;
+  private Long idItinerary;
+  private Long lineId;
+  private String mappedRequest;
+  private String sensAller = "A";
+  private String sensRetour = "R";
+  private String idRetour;
 
-  public String creerItineraireRetour()
+  public Long getIdItineraire()
   {
-    itineraireManager.creerItineraireRetour(itineraireModel.getIdItineraire());
-    addActionMessage(getText("itineraire.retour.ok"));
-    return SUCCESS;
+    return idItinerary;
   }
 
-  public String cancel()
+  public void setIdItineraire(Long idItinerary)
   {
-    addActionMessage(getText("itineraire.cancel.ok"));
-    return SUCCESS;
+    this.idItinerary = idItinerary;
   }
 
-  public String delete()
+  public Long getIdLigne()
   {
-    itineraireManager.supprimer(itineraireModel.getIdItineraire());
-    addActionMessage(getText("itineraire.delete.ok"));
-    return SUCCESS;
+    return lineId;
   }
 
-  @SkipValidation
-  public String edit()
+  public void setIdLigne(Long lineId)
   {
-    List<Itineraire> itinerairesSansItineraireEdite = ligneManager.getItinerairesLigne(itineraireModel.getIdLigne());
-
-    //	Suppression dans la liste des itinéraires de celui étant édité
-    for (Itineraire itineraire : itinerairesSansItineraireEdite)
-    {
-      if (itineraire.getId().equals(itineraireModel.getIdItineraire()))
-      {
-        itinerairesSansItineraireEdite.remove(itineraire);
-        break;
-      }
-    }
-
-    itineraireModel.setItinerairesSansItineraireEdite(itinerairesSansItineraireEdite);
-
-    return INPUT;
+    this.lineId = lineId;
   }
 
+  /********************************************************
+   *                  MODEL + PREPARE                     *
+   ********************************************************/
   @Override
-  @SkipValidation
-  public String input() throws Exception
+  public Itineraire getModel()
   {
-    return INPUT;
+    return itineraryModel;
   }
 
-  public boolean isArretsVide(Long idItineraire)
+  public void prepare() throws Exception
   {
-    return itineraireManager.getArretsItineraire(idItineraire).isEmpty();
+    log.debug("Prepare with id : " + getIdItineraire());
+    if (getIdItineraire() == null)
+    {
+      itineraryModel = new Itineraire();
+    }
+    else
+    {
+      itineraryModel = itineraryManager.lire(getIdItineraire());
+    }
   }
 
-  /**
-   * Renvoit la liste des itinéraires classés avec au début les itinéraires par couple aller
-   * et retour et ensuite les itinéraires sans retour
-   * @return liste des itinéraires classés
-   */
+  /********************************************************
+   *                           CRUD                       *
+   ********************************************************/
   @SkipValidation
   public String list()
   {
     //	Récupération des itinéraires pour un identifiant de ligne donnée
-    List<Itineraire> itinerairesLigne = ligneManager.getItinerairesLigne(itineraireModel.getIdLigne());
+    List<Itineraire> itinerairesLigne = lineManager.getItinerairesLigne(getIdLigne());
     //	Liste des itinéraires classés
     List<Itineraire> itineraires = new ArrayList<Itineraire>();
     //	Liste de tous les ids de retour de la liste des itinéraires initiale
@@ -141,84 +135,203 @@ public class ItineraireAction extends GeneriqueAction implements ModelDriven, Pr
       }
     }
 
-    itineraireModel.setItineraires(itineraires);
-
-    return SUCCESS;
+    this.request.put("itineraires", itineraires);
+    log.debug("List of itineraries");
+    return LIST;
   }
 
   @SkipValidation
-  public void prepare() throws Exception
+  public String add()
   {
-    if (itineraireModel.getIdItineraire() != null)
+    // TODO : Le virer grâce a OGNL
+    List<Itineraire> itinerairesSansItineraireEdite = lineManager.getItinerairesLigne(itineraryModel.getIdLigne());
+
+    //	Suppression dans la liste des itinéraires de celui étant édité
+    for (Itineraire itineraire : itinerairesSansItineraireEdite)
     {
-      itineraireModel.setItineraire(itineraireManager.lire(itineraireModel.getIdItineraire()));
+      if (itineraire.getId().equals(itineraryModel.getId()))
+      {
+        itinerairesSansItineraireEdite.remove(itineraire);
+        break;
+      }
     }
+
+    this.request.put("itinerairesSansItineraireEdite", itinerairesSansItineraireEdite);
+    setMappedRequest(SAVE);
+    return EDIT;
   }
 
-  public void setItineraireManager(IItineraireManager itineraireManager)
+  public String save()
   {
-    this.itineraireManager = itineraireManager;
+    itineraryManager.creer(itineraryModel);
+
+    if (itineraryModel.getIdRetour() != null)
+    {
+      //	Si exitence d'un idRetour on associe cet itinéraire avec son itinéraire retour
+      if (itineraryModel.getIdRetour().equals("-1"))
+      {
+        itineraryManager.dissocierItineraire(itineraryModel.getId());
+      }
+      else
+      {
+        itineraryManager.dissocierItineraire(itineraryModel.getId());
+        itineraryManager.associerItineraire(itineraryModel.getId(), new Long(itineraryModel.getIdRetour()));
+      }
+    }
+
+    setMappedRequest(SAVE);
+    addActionMessage(getText("itineraire.create.ok"));
+    log.debug("Create itinerary with id : " + itineraryModel.getId());
+    return REDIRECTLIST;
   }
 
-  public void setLigneManager(ILigneManager lineManager)
+  @SkipValidation
+  public String edit()
   {
-    this.ligneManager = lineManager;
+    // TODO : Le virer grâce a OGNL
+    List<Itineraire> itinerairesSansItineraireEdite = lineManager.getItinerairesLigne(itineraryModel.getIdLigne());
+
+    //	Suppression dans la liste des itinéraires de celui étant édité
+    for (Itineraire itineraire : itinerairesSansItineraireEdite)
+    {
+      if (itineraire.getId().equals(itineraryModel.getId()))
+      {
+        itinerairesSansItineraireEdite.remove(itineraire);
+        break;
+      }
+    }
+
+    this.request.put("itinerairesSansItineraireEdite", itinerairesSansItineraireEdite);
+    setMappedRequest(UPDATE);
+    return EDIT;
   }
 
   public String update()
   {
-    log.debug("update : " + itineraireModel.getItineraire());
-    Itineraire itineraire = itineraireModel.getItineraire();
-    if (itineraire == null)
-    {
-      return INPUT;
-    }
+    itineraryManager.modifier(itineraryModel);
 
-    // ré-affecter l'identifiant de la ligne sur l'itinéraire
-    //itineraire.setIdLigne(idLigne);
-
-    if (itineraire.getId() == null)
+    if (itineraryModel.getIdRetour() != null)
     {
-      itineraireManager.creer(itineraire);
-      if (itineraireModel.getIdRetour() != null)
+      //	Si exitence d'un idRetour on associe cet itinéraire avec son itinéraire retour
+      if (itineraryModel.getIdRetour().equals("-1"))
       {
-        //	Si exitence d'un idRetour on associe cet itinéraire avec son itinéraire retour
-        if (itineraireModel.getIdRetour().equals("-1"))
-        {
-          itineraireManager.dissocierItineraire(itineraire.getId());
-        }
-        else
-        {
-          itineraireManager.dissocierItineraire(itineraire.getId());
-          itineraireManager.associerItineraire(itineraire.getId(), new Long(itineraireModel.getIdRetour()));
-        }
+        itineraryManager.dissocierItineraire(itineraryModel.getId());
       }
-      addActionMessage(getText("itineraire.create.ok"));
-    }
-    else
-    {
-      itineraireManager.modifier(itineraire);
-      if (itineraireModel.getIdRetour() != null)
+      else
       {
-        //	Si exitence d'un idRetour on associe cet itinéraire avec son itinéraire retour
-        if (itineraireModel.getIdRetour().equals("-1"))
-        {
-          itineraireManager.dissocierItineraire(itineraire.getId());
-        }
-        else
-        {
-          itineraireManager.dissocierItineraire(itineraire.getId());
-          itineraireManager.associerItineraire(itineraire.getId(), new Long(itineraireModel.getIdRetour()));
-        }
+        itineraryManager.dissocierItineraire(itineraryModel.getId());
+        itineraryManager.associerItineraire(itineraryModel.getId(), new Long(itineraryModel.getIdRetour()));
       }
-      addActionMessage(getText("itineraire.update.ok"));
     }
 
+    setMappedRequest(UPDATE);
+    addActionMessage(getText("itineraire.update.ok"));
+    log.debug("Update itinerary with id : " + itineraryModel.getId());
+    return REDIRECTLIST;
+  }
+
+  public String delete()
+  {
+    itineraryManager.supprimer(getModel().getId());
+    addActionMessage(getText("itineraire.delete.ok"));
+    log.debug("Delete itinerary with id : " + getModel().getId());
+    return REDIRECTLIST;
+  }
+
+  @SkipValidation
+  public String cancel()
+  {
+    addActionMessage(getText("itineraire.cancel.ok"));
+    return REDIRECTLIST;
+  }
+
+  @Override
+  @SkipValidation
+  public String input() throws Exception
+  {
+    return INPUT;
+  }
+
+  /********************************************************
+   *                        MANAGER                       *
+   ********************************************************/
+  public void setItineraireManager(IItineraireManager itineraryManager)
+  {
+    ItineraireAction.itineraryManager = itineraryManager;
+  }
+
+  public void setLigneManager(ILigneManager lineManager)
+  {
+    ItineraireAction.lineManager = lineManager;
+  }
+
+  /********************************************************
+   *                   METHODE ACTION                     *
+   ********************************************************/
+  // this prepares command for button on initial screen write
+  public void setMappedRequest(String actionMethod)
+  {
+    this.mappedRequest = actionMethod;
+  }
+
+  // when invalid, the request parameter will restore command action
+  public void setActionMethod(String method)
+  {
+    this.mappedRequest = method;
+  }
+
+  public String getActionMethod()
+  {
+    return mappedRequest;
+  }
+
+  public String creerItineraireRetour()
+  {
+    itineraryManager.creerItineraireRetour(itineraryModel.getId());
+    addActionMessage(getText("itineraire.retour.ok"));
     return SUCCESS;
   }
 
-  public Object getModel()
+  public boolean isArretsVide(Long idItineraire)
   {
-    return itineraireModel;
+    return itineraryManager.getArretsItineraire(idItineraire).isEmpty();
+  }
+
+  public String getSensItineraire()
+  {
+    if (itineraryModel != null && itineraryModel.getWayBack() != null)
+    {
+      return itineraryModel.getWayBack().toString();
+    }
+    else
+    {
+      return sensAller;
+    }
+  }
+
+  public Map<String, String> getSensItineraires()
+  {
+    Map<String, String> sens = new HashMap<String, String>();
+    sens.put(sensRetour, sensRetour);
+    sens.put(sensAller, sensAller);
+    return sens;
+  }
+
+  public String getIdRetour()
+  {
+    //log.debug("this.itineraire : " + this.itineraire);
+    if (this.itineraryModel.getIdRetour() == null)
+    {
+      return "-1";
+    }
+    else
+    {
+      return this.itineraryModel.getIdRetour().toString();
+    }
+  }
+
+  public void setIdRetour(String idRetour)
+  {
+    this.idRetour = idRetour;
   }
 }
