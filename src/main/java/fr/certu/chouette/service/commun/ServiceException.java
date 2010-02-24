fbbd0db1ff71b89fce0666bac2909b89f8ac0434
Michel Etienne <michel@garfield.(none)>
2010-02-24 11:17:52 +0100

    mise a point message exception

--- src/main/java/fr/certu/chouette/service/commun/ServiceException.java
+++ src/main/java/fr/certu/chouette/service/commun/ServiceException.java
@@ -7,10 +7,10 @@ import java.util.ResourceBundle;
 
 public  class ServiceException extends RuntimeException 
 {
-   
+
 	private CodeIncident code;
 	private CodeDetailIncident detail;
-   private Object[] args;
+	private Object[] args;
 
 	public ServiceException(CodeIncident code, CodeDetailIncident detail, Object...args ) 
 	{
@@ -21,75 +21,82 @@ public  class ServiceException extends RuntimeException
 	}
 	public ServiceException(CodeIncident code, CodeDetailIncident detail, Throwable exception,Object...args) 
 	{
-      super("",exception);
-      this.code = code;
-      this.detail = detail;
-      this.args = args;
+		super("",exception);
+		this.code = code;
+		this.detail = detail;
+		this.args = args;
 	}
 	/*
    public ServiceException(CodeIncident code, Object...args ) 
    {
       this(code,CodeDetailIncident.DEFAULT,args);
    }
-   */   
-   public ServiceException(CodeIncident code, Throwable exception, Object...args) 
-   {
-      this(code,CodeDetailIncident.DEFAULT,exception,args);
-   }
-   
-   
-   public ServiceException(CodeIncident code, Throwable exception) 
-   {
-      this(code,CodeDetailIncident.DEFAULT,exception,new Object[0]);
-   }
+	 */   
+	public ServiceException(CodeIncident code, Throwable exception, Object...args) 
+	{
+		this(code,CodeDetailIncident.DEFAULT,exception,args);
+	}
 
-   public ServiceException(CodeIncident code) 
-   {
-      this(code,CodeDetailIncident.DEFAULT,new Object[0]);
-   }
+
+	public ServiceException(CodeIncident code, Throwable exception) 
+	{
+		this(code,CodeDetailIncident.DEFAULT,exception,new Object[0]);
+	}
+
+	public ServiceException(CodeIncident code) 
+	{
+		this(code,CodeDetailIncident.DEFAULT,new Object[0]);
+	}
 
 
 
 	public CodeIncident getCode() {
 		return code;
 	}
-   public CodeDetailIncident getDetailCode() {
-      return detail;
-   }
+	public CodeDetailIncident getDetailCode() {
+		return detail;
+	}
 
-   /* (non-Javadoc)
-    * @see java.lang.Throwable#getMessage()
-    */
-   @Override
-   public String getMessage()
-   {
-      ResourceBundle bundle = ResourceBundle.getBundle(this.getClass().getName(),Locale.getDefault());
-      String format = "";
-      try
-      {
-         format = bundle.getString("msg."+code.name()+"."+detail.name());
-      }
-      catch (MissingResourceException e)
-      {
-         format = bundle.getString("msg.noMsg");
-         args = new Object[]{code.name(),detail.name()};
-      }
-      
-      String message = MessageFormat.format(format,args);
-      if (this.getCause() != null)
-      {
-         format = bundle.getString("msg.cause");
-         message += "\n"+MessageFormat.format(format,this.getCause().getLocalizedMessage());
-      }
-      return message;
-   }
-   /* (non-Javadoc)
-    * @see java.lang.Throwable#getLocalizedMessage()
-    */
-   @Override
-   public String getLocalizedMessage()
-   {
-      return getMessage();
-   }
-   
+	/* (non-Javadoc)
+	 * @see java.lang.Throwable#getMessage()
+	 */
+	@Override
+	public String getMessage()
+	{
+		try
+		{
+			ResourceBundle bundle = ResourceBundle.getBundle("ServiceException",Locale.getDefault());
+			String format = "";
+			try
+			{
+				format = bundle.getString("msg."+code.name()+"."+detail.name());
+			}
+			catch (MissingResourceException e)
+			{
+				format = bundle.getString("msg.noMsg");
+				args = new Object[]{code.name(),detail.name()};
+			}
+
+			String message = MessageFormat.format(format,args);
+			if (this.getCause() != null)
+			{
+				format = bundle.getString("msg.cause");
+				message += "\n"+MessageFormat.format(format,this.getCause().getLocalizedMessage());
+			}
+			return message;
+		}
+		catch (Exception ex)
+		{
+			return this.getClass().getName() + ":"+ code.name()+" "+detail.name();
+		}
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Throwable#getLocalizedMessage()
+	 */
+	@Override
+	public String getLocalizedMessage()
+	{
+		return getMessage();
+	}
+
 }
