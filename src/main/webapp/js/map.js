c77f0e62a17094c9565aae1d93fc5647dd68a1b3
Jérémie GASTON-RAOUL <jeremie.gaston-raoul@dryade.net>
2010-08-06 18:07:05 +0200

    add functions for stopplace map

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -1,12 +1,18 @@
-var map, drawControls, stopPlaceMarker, vectorLayer;
+//define global vars
+var map, drawControls, stopPlaceMarker, editMarkerLayer, showMarkerLayer, childrenAreas;
 
+//define Projection "Lambert II Etendu" 
 Proj4js.defs['EPSG:27582'] = "+title=NTF (Paris) / France II (deprecated) +proj=lcc +lat_1=46.8 +lat_0=46.8 +lon_0=0 +k_0=0.99987742 +x_0=600000 +y_0=2200000 +a=6378249.2 +b=6356515 +towgs84=-168,-60,320,0,0,0,0 +pm=paris +units=m";
 
+
+// === INIT MAP PROJECTIONS ===
 var wgsProjection= new OpenLayers.Projection("EPSG:4326");
 var lambertProjection= new OpenLayers.Projection("EPSG:27582");
 var geoportalProjection= new OpenLayers.Projection("IGNF:GEOPORTALFXX");
 
 function init(){
+	
+	// === INIT MAP ===
 	map = new OpenLayers.Map('map', {
 		resolutions: Geoportal.Catalogue.RESOLUTIONS,
 		projection: geoportalProjection,
@@ -22,7 +28,10 @@ function init(){
 			new OpenLayers.Control.LoadingPanel()
 		]
 	});
-	 
+	
+	// === INIT LAYERS ===
+	
+	//geographic map layer
 	var geoMapLayer= new Geoportal.Layer.WMSC(
 		"Plan",
 		gGEOPORTALRIGHTSMANAGEMENT[gGEOPORTALRIGHTSMANAGEMENT.apiKey].resources['GEOGRAPHICALGRIDSYSTEMS.MAPS:WMSC'].url,
@@ -34,7 +43,7 @@ function init(){
 		{
 			gridOrigin: new OpenLayers.LonLat(0,0),
 			isBaseLayer: true,
-			resolutions: Geoportal.Catalogue.RESOLUTIONS.slice(5,15),
+			resolutions: Geoportal.Catalogue.RESOLUTIONS.slice(5,18),
 			alwaysInRange: true,
 			projection: geoportalProjection,
 			maxExtent: new OpenLayers.Bounds(-180, -57, 180, 72).transform(wgsProjection,geoportalProjection, true),
@@ -48,6 +57,7 @@ function init(){
 		}
 	);
 	
+	// orthophoto layer
 	var orthoPhotoLayer= new Geoportal.Layer.WMSC(
 		"Satellite",
 		gGEOPORTALRIGHTSMANAGEMENT[gGEOPORTALRIGHTSMANAGEMENT.apiKey].resources['ORTHOIMAGERY.ORTHOPHOTOS:WMSC'].url,
@@ -59,7 +69,7 @@ function init(){
 		{
 			gridOrigin: new OpenLayers.LonLat(0,0),
 			isBaseLayer: true,
-			resolutions: Geoportal.Catalogue.RESOLUTIONS.slice(5,15),
+			resolutions: Geoportal.Catalogue.RESOLUTIONS.slice(5,18),
 			alwaysInRange: true,
 			projection: geoportalProjection,
 			maxExtent: new OpenLayers.Bounds(-180, -57, 180, 72).transform(wgsProjection,geoportalProjection, true),
@@ -73,16 +83,41 @@ function init(){
 		}
 	);
 	
-	var symbolizer = OpenLayers.Util.applyDefaults(
+	// edit marker layer
+	var editMarkerSymbolizer = OpenLayers.Util.applyDefaults(
         {externalGraphic: "../js/openlayers/img/green_round_marker.png", pointRadius: 10, fillOpacity: 1},
         OpenLayers.Feature.Vector.style["default"]);
-	var styleMap = new OpenLayers.StyleMap({"default": symbolizer, "select": {}});
+	var editMarkerStyleMap = new OpenLayers.StyleMap({"default": editMarkerSymbolizer, "select": {}});
 	
-	vectorLayer = new OpenLayers.Layer.Vector("Vector Layer", {styleMap: styleMap});
+	editMarkerLayer = new OpenLayers.Layer.Vector(
+		"Edit Marker Layer",
+		{
+			styleMap: editMarkerStyleMap,
+			displayInLayerSwitcher: false
+		}
+	);
+	
+	//show marker layer
+	var showMarkerSymbolizer = OpenLayers.Util.applyDefaults(
+        {externalGraphic: "../js/openlayers/img/marker-blue.png", pointRadius: 20, fillOpacity: 1},
+        OpenLayers.Feature.Vector.style["default"]);
+	var showMarkerStyleMap = new OpenLayers.StyleMap({"default": showMarkerSymbolizer, "select": {}});
+	
+	
+	showMarkerLayer = new OpenLayers.Layer.Vector(
+		"Show Marker Layer",
+		{
+			styleMap: showMarkerStyleMap,
+			displayInLayerSwitcher: false
+		}
+	);
 
-	//OpenLayers.Feature.Vector.style['default']['strokeWidth'] = '2';
-	
-	vectorLayer.events.on({
+	map.addLayers([geoMapLayer,orthoPhotoLayer,editMarkerLayer,showMarkerLayer]);
+	map.setCenter(new OpenLayers.LonLat(177169.0,5441595.0),10);
+
+	// === INIT EVENTS MANAGEMENT ===
+
+	editMarkerLayer.events.on({
 //		"beforefeaturemodified": report,
 //		"afterfeaturemodified": report,
 //		"sketchmodified": report,
@@ -91,39 +126,34 @@ function init(){
 		"featuremodified": report,
 		"sketchcomplete": report
 	});
+		
+	// === INIT CONTROLS ===
 	
-	map.addLayers([geoMapLayer,orthoPhotoLayer,vectorLayer]);
-	map.setCenter(new OpenLayers.LonLat(177169.0,5441595.0),10);
+	var highlightCtrl = new OpenLayers.Control.SelectFeature(showMarkerLayer, { 
+		hover: true, highlightOnly: true, renderIntent: "",
+		eventListeners: { featurehighlighted: showTooltipOnEvent, featureunhighlighted: hideTooltipOnEvent}
+	});
 	
+	map.addControl(highlightCtrl);
+	highlightCtrl.activate();
 	
 	drawControls = {
-		draw: new OpenLayers.Control.DrawFeature(vectorLayer,OpenLayers.Handler.Point),
-		modify: new OpenLayers.Control.ModifyFeature(vectorLayer,{clickout: false})
+		draw: new OpenLayers.Control.DrawFeature(editMarkerLayer,OpenLayers.Handler.Point),
+		modify: new OpenLayers.Control.ModifyFeature(editMarkerLayer,{clickout: false})
 	};
 	
 	for(var key in drawControls) {
 		map.addControl(drawControls[key]);
 	}
 	
+	initShowMarkerLayer();
 	updateStopPlaceMarker();
 };
 
-function report(event) {
-	//console.log(event.type, event.feature ? event.feature.id : event.components);
-	if(event.type == "sketchcomplete"){
-		stopPlaceMarker = event.feature;
-		updateCoordsFields(stopPlaceMarker);
-		drawControls.draw.deactivate();
-		drawControls.modify.activate();
-		vectorLayer.addFeatures([stopPlaceMarker]);
-		drawControls.modify.selectControl.select(stopPlaceMarker);
-		//prevents API from adding feature to the layer a second time
-		return false;
-	}
-	else if(event.type == "featuremodified" || event.type == "vertexmodified"){
-		updateCoordsFields();
-	}
-};
+
+/////////////////////////
+// CONTROLS MANAGEMENT //
+/////////////////////////
 
 function toggleDrawControl(element) {
 	for(key in controls) {
@@ -135,10 +165,51 @@ function toggleDrawControl(element) {
 		}
 		else{
 			drawControls.modify.activate();
-			drawControls.modify.selectControl.select(vectorLayer.features[0]);
+			drawControls.modify.selectControl.select(editMarkerLayer.features[0]);
 		}
 	}
 };
+
+
+//////////////////////
+// EVENT MANAGEMENT //
+//////////////////////
+
+function report(event) {
+	//console.log(event.type, event.feature ? event.feature.id : event.components);
+	if(event.type == "sketchcomplete"){
+		stopPlaceMarker = event.feature;
+		updateCoordsFieldsFromMarker(stopPlaceMarker);
+		drawControls.draw.deactivate();
+		drawControls.modify.activate();
+		editMarkerLayer.addFeatures([stopPlaceMarker]);
+		drawControls.modify.selectControl.select(stopPlaceMarker);
+		//prevents API from adding feature to the layer a second time
+		return false;
+	}
+	else if(event.type == "featuremodified" || event.type == "vertexmodified"){
+		updateCoordsFieldsFromMarker();
+	}
+};
+
+function showTooltipOnEvent(event){
+	var feature = event.feature;
+	console.log("test event");
+	if(feature.renderIntent != "select"){
+		map.addPopup(feature.popup);
+	}
+};
+
+function hideTooltipOnEvent(event){
+	var feature = event.feature;
+	console.log("youhou");
+	map.removePopup(feature.popup);
+};
+
+
+//////////////////////////////////
+// STOP PLACE MARKER MANAGEMENT // 
+//////////////////////////////////
 
 function updateStopPlaceMarker(){
 	if($("stoparea_latitude").value != "" && $("stoparea_longitude").value != ""){
@@ -150,13 +221,13 @@ function updateStopPlaceMarker(){
 			stopPlaceMarker = new OpenLayers.Feature.Vector(new OpenLayers.Geometry.Point($("stoparea_longitude").value,$("stoparea_latitude").value).transform(wgsProjection,geoportalProjection));
 			drawControls.draw.deactivate();
 			drawControls.modify.activate();
-			vectorLayer.addFeatures([stopPlaceMarker]);
+			editMarkerLayer.addFeatures([stopPlaceMarker]);
 			drawControls.modify.selectControl.select(stopPlaceMarker);
 		}
 	}
 	else{
 		if(stopPlaceMarker != null){
-			vectorLayer.removeFeatures([stopPlaceMarker]);
+			editMarkerLayer.removeFeatures([stopPlaceMarker]);
 			stopPlaceMarker = null;
 		}
 		drawControls.modify.deactivate();
@@ -169,7 +240,32 @@ function updateStopPlaceMarker(){
 	return false;
 };
 
-function updateCoordsFields(){
+function centerOnMarker(){
+	if(stopPlaceMarker != null){
+		var geom = stopPlaceMarker.geometry;
+		var newCoords = new OpenLayers.LonLat(geom.x,geom.y);
+		map.panTo(newCoords);
+	}
+};
+
+function barycentreStopPlaceMarker(){
+	var childrenPoints = showMarkerLayer.features.collect(function(feature){
+		return feature.geometry;
+	});
+	var point = barycentre(childrenPoints);
+	
+	$("stoparea_latitude").value=point.y.toFixed(6);
+	$("stoparea_longitude").value=point.x.toFixed(6);
+	
+	updateXYFieldsCoordsFromLatLon();
+	updateStopPlaceMarker();
+};
+
+////////////////////////////
+// FORM FIELDS MANAGEMENT //
+////////////////////////////
+
+function updateCoordsFieldsFromMarker(){
 	var point = stopPlaceMarker.geometry.clone();
 	
 	point.transform(geoportalProjection,wgsProjection);
@@ -181,13 +277,13 @@ function updateCoordsFields(){
 	$("stoparea_y").value=point.y.toFixed(2);
 };
 
-function updateLatLngFieldsCoords(){
+function updateLatLonFieldsCoordsFromXY(){
 	var coords = new OpenLayers.LonLat($("stoparea_x").value,$("stoparea_y").value).transform(lambertProjection,wgsProjection);
 	$("stoparea_latitude").value=coords.lat.toFixed(6);
 	$("stoparea_longitude").value=coords.lon.toFixed(6);
 };
 
-function updateXYFieldsCoords(){
+function updateXYFieldsCoordsFromLatLon(){
 	var coords = new OpenLayers.LonLat($("stoparea_longitude").value,$("stoparea_latitude").value).transform(wgsProjection,lambertProjection);
 	$("stoparea_x").value=coords.lon.toFixed(2);
 	$("stoparea_y").value=coords.lat.toFixed(2);
@@ -205,7 +301,7 @@ function updateCoordsFrom(field){
 			else{
 				$("stoparea_x").value = x.toFixed(2);
 				if($("stoparea_y").value != ""){
-					updateLatLngFieldsCoords();
+					updateLatLonFieldsCoordsFromXY();
 				}
 			}
 			break;
@@ -219,7 +315,7 @@ function updateCoordsFrom(field){
 			else{
 				$("stoparea_y").value = y.toFixed(2);
 				if($("stoparea_x").value != ""){
-					updateLatLngFieldsCoords();
+					updateLatLonFieldsCoordsFromXY();
 				}
 			}
 			break;
@@ -233,7 +329,7 @@ function updateCoordsFrom(field){
 			else{
 				$("stoparea_latitude").value = lat.toFixed(6);
 				if($("stoparea_longitude").value != ""){
-					updateXYFieldsCoords();;
+					updateXYFieldsCoordsFromLatLon();
 				}
 			}
 			break;
@@ -247,7 +343,7 @@ function updateCoordsFrom(field){
 			else{
 				$("stoparea_longitude").value = lon.toFixed(6);
 				if($("stoparea_latitude").value != ""){
-					updateXYFieldsCoords();
+					updateXYFieldsCoordsFromLatLon();
 				}
 			}
 			break;
@@ -258,13 +354,52 @@ function updateCoordsFrom(field){
 	updateStopPlaceMarker();	
 };
 
-function centerOnMarker(){
-	if(stopPlaceMarker != null){
-		var geom = stopPlaceMarker.geometry;
-		var newCoords = new OpenLayers.LonLat(geom.x,geom.y);
-		map.panTo(newCoords);
+//////////////////////////////////
+// SHOW MARKER LAYER MANAGEMENT //
+//////////////////////////////////
+
+function initShowMarkerLayer(){
+	if(	$("stoparea_idPositionGeographique").value != null){
+		var url = "../json/JSONStopPlace?stopPlaceId="+$("stoparea_idPositionGeographique").value;
+		
+		new Ajax.Request(url, {
+			method: 'get',
+			onSuccess: function(transport) {
+				childrenAreas = eval(transport.responseText);
+				childrenAreas.each(function(area){
+					if(area.latitude != null && area.longitude != null){
+						var markPoint = new OpenLayers.Geometry.Point(area.longitude, area.latitude);
+						var popup = new OpenLayers.Popup("tooltip", 
+							markPoint.getBounds().getCenterLonLat(),
+							null,
+							"<div class='tooltip'>"+area.name+"</div>",
+							false);
+						popup.autoSize = true;
+						var mark = new OpenLayers.Feature.Vector(markPoint.transform(wgsProjection,geoportalProjection), {'area':area, 'popup' : popup});
+						showMarkerLayer.addFeatures([mark]);
+					}
+				});
+			}
+		});
 	}
 };
 
+
+////////////////
+// MATH TOOLS //
+////////////////
+
+function barycentre(points){
+	var x = 0;
+	var y = 0;
+	points.each(function(point){
+		x+=point.x;
+		y+=point.y;
+		console.log(x+" "+y);
+	});
+	x = x/points.length;
+	y = y/points.length;
+	return new OpenLayers.Geometry.Point(x,y);
+};
 
 window.onload = init;
