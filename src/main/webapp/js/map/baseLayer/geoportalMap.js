ae85f16b45d89dd4f2ba43f2d2b1906aac4b8b4c
Jérémie GASTON-RAOUL <jeremie.gaston-raoul@dryade.net>
2010-09-15 16:44:45 +0200

    add optional layers in Geoportal map

--- src/main/webapp/js/map/baseLayer/geoportalMap.js
+++ src/main/webapp/js/map/baseLayer/geoportalMap.js
@@ -2,27 +2,42 @@
 Chouette.Map.baseLayerProjection = new OpenLayers.Projection("IGNF:GEOPORTALFXX");
 
 Chouette.Map.initBaseLayers = function(){
-  var geoMapLayer= this.createGeoportalLayer("GEOGRAPHICALGRIDSYSTEMS.MAPS", "Plan");
-  // orthophoto layer
-  var orthoPhotoLayer= this.createGeoportalLayer("ORTHOIMAGERY.ORTHOPHOTOS", "Satellite");
+  //map layer 
+  var geoMapLayer= this.createGeoportalBaseLayer("GEOGRAPHICALGRIDSYSTEMS.MAPS", "Plan");
+  //orthophoto layer
+  var orthoPhotoLayer= this.createGeoportalBaseLayer("ORTHOIMAGERY.ORTHOPHOTOS", "Satellite");
 
-   this.map.addLayers([geoMapLayer,orthoPhotoLayer]);
+  var railwaysLayer= this.createGeoportalOptionalLayer("TRANSPORTNETWORKS.RAILWAYS", "RailsWays");
+  var roadsLayer= this.createGeoportalOptionalLayer("TRANSPORTNETWORKS.ROADS", "Roads");
+  var buildingsLayer= this.createGeoportalOptionalLayer("BUILDINGS.BUILDINGS", "Buildings");
+
+
+   this.map.addLayers([geoMapLayer,orthoPhotoLayer,railwaysLayer,roadsLayer,buildingsLayer]);
 };
 
-Chouette.Map.createGeoportalLayer = function(layerType, layerName)
+Chouette.Map.createGeoportalBaseLayer = function(layerType, layerName){
+	return this.createGeoportalLayer(layerType, layerName, true, false, true,0);
+};
+
+Chouette.Map.createGeoportalOptionalLayer = function(layerType, layerName){
+	return this.createGeoportalLayer(layerType, layerName, false, true, false,10);
+};
+
+Chouette.Map.createGeoportalLayer = function(layerType, layerName, isBaseLayer, isTransparent, isVisible)
 {
   return new Geoportal.Layer.WMSC(
     layerName,
     gGEOPORTALRIGHTSMANAGEMENT[gGEOPORTALRIGHTSMANAGEMENT.apiKey].resources[layerType + ':WMSC'].url,
     {
       layers: layerType,
-      format:'image/jpeg',
-      exceptions:"text/xml"
+      exceptions:"text/xml",
+      transparent:isTransparent
     },
     {
       gridOrigin: new OpenLayers.LonLat(0,0),
-      isBaseLayer: true,
+      isBaseLayer: isBaseLayer,
       resolutions: Geoportal.Catalogue.RESOLUTIONS.slice(5,18),
+      visibility: isVisible,
       alwaysInRange: true,
       projection: this.baseLayerProjection,
       units: this.baseLayerProjection.getUnits(),
