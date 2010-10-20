f90f5a84cf43cdc4e3f4520690b508e123554e6a
Jérémie GASTON-RAOUL <jeremie.gaston-raoul@dryade.net>
2010-10-20 17:08:06 +0200

    avoid jsonLine to fail when a physical stoppoint has no parent

--- src/main/java/fr/certu/chouette/struts/json/JSONLineAction.java
+++ src/main/java/fr/certu/chouette/struts/json/JSONLineAction.java
@@ -4,13 +4,18 @@ import fr.certu.chouette.modele.PositionGeographique;
 import fr.certu.chouette.service.database.ILigneManager;
 import fr.certu.chouette.service.database.IPositionGeographiqueManager;
 import fr.certu.chouette.struts.GeneriqueAction;
+import fr.certu.chouette.struts.stopArea.StopAreaAction;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class JSONLineAction extends GeneriqueAction
 {
-
+  private static final Log log = LogFactory.getLog(JSONLineAction.class);
   private ILigneManager ligneManager;
   private Long lineId;
   private IPositionGeographiqueManager positionGeographiqueManager;
@@ -27,19 +32,21 @@ public class JSONLineAction extends GeneriqueAction
 
   public Set<PositionGeographique> getStopPlaces()
   {
+	log.error("getStopPlaces");
+
     List<PositionGeographique> boardingPositionsOnRoute = ligneManager.getArretsPhysiques(lineId);
     Set<PositionGeographique> stopPlaces = new HashSet<PositionGeographique>();
 
     for (PositionGeographique positionGeographique : boardingPositionsOnRoute)
     {
-      PositionGeographique stopPlace = positionGeographiqueManager.lire(positionGeographique.idParent);
-
-      if (stopPlace != null)
-      {
-        stopPlaces.add(stopPlace);
+      if(positionGeographique.idParent != null){
+        PositionGeographique stopPlace = positionGeographiqueManager.lire(positionGeographique.idParent);
+        if (stopPlace != null)
+        {
+          stopPlaces.add(stopPlace);
+        }
       }
     }
-
     return stopPlaces;
   }
 
