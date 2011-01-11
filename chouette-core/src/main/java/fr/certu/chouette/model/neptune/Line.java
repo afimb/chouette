d1773d4c06da3c38d063d8a7a8f341fb53dd165a
Jérémie GASTON-RAOUL <jeremie.gaston-raoul@dryade.net>
2011-01-11 13:37:04 +0100

    remove some useless imports

--- chouette-core/src/main/java/fr/certu/chouette/model/neptune/Line.java
+++ chouette-core/src/main/java/fr/certu/chouette/model/neptune/Line.java
@@ -8,9 +8,7 @@
 package fr.certu.chouette.model.neptune;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
