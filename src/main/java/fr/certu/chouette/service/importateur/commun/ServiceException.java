cc7b8751e06ae75abc0b323455c8c39741cb81f9
Zakaria BOUZIANE <zbouziane@treize.(none)>
2011-01-24 15:20:13 +0100

    undeling files

---
+++ src/main/java/fr/certu/chouette/service/importateur/commun/ServiceException.java
@@ -0,0 +1,21 @@
+package fr.certu.chouette.service.importateur.commun;
+
+@SuppressWarnings("serial")
+public class ServiceException extends RuntimeException {
+    
+    private CodeIncident code;
+
+    public ServiceException(CodeIncident code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public ServiceException(CodeIncident code, String message, Throwable exception) {
+        super(message, exception);
+        this.code = code;
+    }
+
+    public CodeIncident getCode() {
+        return code;
+    }
+}
