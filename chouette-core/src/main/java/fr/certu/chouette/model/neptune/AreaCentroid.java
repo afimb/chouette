667b50132a6bfcbe0818375a2f2943651d74c504
Jérémie GASTON-RAOUL <jeremie.gaston-raoul@dryade.net>
2011-02-08 09:22:06 +0100

    add class members to AreaCentroid

--- chouette-core/src/main/java/fr/certu/chouette/model/neptune/AreaCentroid.java
+++ chouette-core/src/main/java/fr/certu/chouette/model/neptune/AreaCentroid.java
@@ -1,5 +1,20 @@
 package fr.certu.chouette.model.neptune;
 
-public class AreaCentroid extends NeptuneIdentifiedObject {
+import java.math.BigDecimal;
 
+import fr.certu.chouette.model.neptune.type.Address;
+import fr.certu.chouette.model.neptune.type.LongLatTypeEnum;
+import fr.certu.chouette.model.neptune.type.ProjectedPoint;
+import lombok.Getter;
+import lombok.Setter;
+
+public class AreaCentroid extends NeptuneIdentifiedObject {
+	@Getter @Setter private Address address;
+	@Getter @Setter private LongLatTypeEnum longLatType;
+	@Getter @Setter private BigDecimal latitude;
+	@Getter @Setter private BigDecimal longitude;
+	@Getter @Setter private ProjectedPoint projectedPoint;
+	@Getter @Setter private String comment;
+	@Getter @Setter private String containedInStopAreaId;
+	@Getter @Setter private StopArea containedInStopArea;
 }
