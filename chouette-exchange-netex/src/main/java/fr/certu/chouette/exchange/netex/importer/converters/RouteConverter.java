ba33bdbf0d1147a175bb3142d4f6fd251383b276
Marc Florisson <marc.florisson@dryade.net>
2013-03-22 15:25:39 +0100

    add StopPoint in RouteConverter

--- chouette-exchange-netex/src/main/java/fr/certu/chouette/exchange/netex/importer/converters/RouteConverter.java
+++ chouette-exchange-netex/src/main/java/fr/certu/chouette/exchange/netex/importer/converters/RouteConverter.java
@@ -6,8 +6,13 @@ import com.ximpleware.VTDNav;
 import com.ximpleware.XPathEvalException;
 import com.ximpleware.XPathParseException;
 import fr.certu.chouette.model.neptune.Route;
+import fr.certu.chouette.model.neptune.StopPoint;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.log4j.Logger;
 
 public class RouteConverter extends GenericConverter 
@@ -15,6 +20,7 @@ public class RouteConverter extends GenericConverter
     private static final Logger       logger = Logger.getLogger(RouteConverter.class);
     private List<Route> routes = new ArrayList<Route>();    
     private AutoPilot autoPilot;
+    private Map<String,StopPoint> stopPointByObjectId;
     private VTDNav nav;
     
     public RouteConverter(VTDNav vTDNav) throws XPathParseException, XPathEvalException, NavException
@@ -23,6 +29,8 @@ public class RouteConverter extends GenericConverter
         
         autoPilot = new AutoPilot(nav);
         autoPilot.declareXPathNameSpace("netex","http://www.netex.org.uk/netex");        
+        
+        stopPointByObjectId = new HashMap<String, StopPoint>();
     }
     
     public List<Route> convert() throws XPathEvalException, NavException, XPathParseException
@@ -41,10 +49,50 @@ public class RouteConverter extends GenericConverter
             route.setPublishedName(parseOptionnalAttribute(nav, "DirectionRef", "ref"));
             route.setObjectVersion(Integer.parseInt(parseOptionnalAttribute(nav, "version")));
             
+
+            List<String> pointOnRouteIds = parseMandatoryAttributes(nav, "PointOnRoute", "id");
+            
+            for( String pointOnRouteId : pointOnRouteIds) {
+                StopPoint stopPoint = new StopPoint();
+                stopPoint.setPosition( pointOnRouteIds.indexOf( pointOnRouteId));
+                stopPoint.setObjectId( stopPointObjectId( route, pointOnRouteId));
+                
+                stopPointByObjectId.put( stopPoint.getObjectId(), stopPoint);
+                route.addStopPoint( stopPoint);
+            }
+            
             routes.add(route);
         } 
         
+        // lecture des PassengerStopAssignment
+        AutoPilot autoPilot2 = new AutoPilot(nav);
+        autoPilot2.declareXPathNameSpace("netex","http://www.netex.org.uk/netex");   
+        autoPilot2.selectXPath("//netex:ServiceFrame/netex:stopAssignments/"+
+                "netex:PassengerStopAssignment");
+        
+        while( (result = autoPilot2.evalXPath()) != -1 )
+        {  
+            String stopPointId = parseOptionnalAttribute(nav, "ScheduledStopPointRef", "ref");
+            String quayId = parseOptionnalAttribute(nav, "QuayRef", "ref");
+            
+            StopPoint stopPoint = stopPointByObjectId.get( stopPointId);
+            stopPoint.setContainedInStopAreaId( quayId);
+        }
+        
+        
         return routes;
+    }
+    
+    public String readStopPointObjectIdFromPointOnRouteId( String pointOnRouteId) {
+        Matcher m = Pattern.compile( "\\S+:\\S+:(\\S+)-\\d+$").matcher(pointOnRouteId);
+        if ( ! m.matches()) {
+            throw new RuntimeException( "PointOnRoute.id "+pointOnRouteId);
+        }
+        return m.group(1);
+    }
+    public String stopPointObjectId(Route route, String pointOnRouteId) {
+        return route.objectIdPrefix()+":StopPoint:"+
+                readStopPointObjectIdFromPointOnRouteId(pointOnRouteId);
     }
     
 }
