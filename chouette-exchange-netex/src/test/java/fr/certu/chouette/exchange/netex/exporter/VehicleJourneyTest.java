c011513f828764b7b9d329f9f0a149ee65a5b7b2
Marc Florisson <marc.florisson@dryade.net>
2013-04-17 14:34:11 +0200

    add more test on VehicleJourneyTest

--- chouette-exchange-netex/src/test/java/fr/certu/chouette/exchange/netex/exporter/VehicleJourneyTest.java
+++ chouette-exchange-netex/src/test/java/fr/certu/chouette/exchange/netex/exporter/VehicleJourneyTest.java
@@ -4,6 +4,7 @@
  */
 package fr.certu.chouette.exchange.netex.exporter;
 
+import fr.certu.chouette.model.neptune.Timetable;
 import fr.certu.chouette.model.neptune.VehicleJourney;
 
 import java.text.ParseException;
@@ -24,6 +25,125 @@ public class VehicleJourneyTest extends ChouetteModelTest {
                                 "netex:ServiceJourney"+
                                 "[@id = '"+modelTranslator.netexId(vehicleJourney)+"'])";
             assertXPathTrue( xPathExpr);
+        }
+    }
+    
+    @Test(groups = {"TimetableFrame", "vehicleJourneys"}, description = "Validate presence of ServiceJourney with expected publishedJourneyName")
+    public void verifyPublishedJourneyName() throws XPathExpressionException, ParseException {
+        
+        List<VehicleJourney> vehicleJourneys = line.getVehicleJourneys();
+        
+        for( VehicleJourney vehicleJourney : vehicleJourneys) {
+           String xPathExpr = "boolean(//netex:TimetableFrame/netex:vehicleJourneys/"+
+                                "netex:ServiceJourney"+
+                                "[@id = '"+modelTranslator.netexId(vehicleJourney)+"']/" +
+                                "netex:Name/"+
+                                "text() = '"+vehicleJourney.getPublishedJourneyName()+"')";
+            assertXPathTrue( xPathExpr);
+        }
+    }
+    
+    @Test(groups = {"TimetableFrame", "vehicleJourneys"}, description = "Validate presence of ServiceJourney with expected publishedJourneyIdentifier")
+    public void verifyPublishedJourneyIdentifier() throws XPathExpressionException, ParseException {
+        
+        List<VehicleJourney> vehicleJourneys = line.getVehicleJourneys();
+        
+        for( VehicleJourney vehicleJourney : vehicleJourneys) {
+           String xPathExpr = "boolean(//netex:TimetableFrame/netex:vehicleJourneys/"+
+                                "netex:ServiceJourney"+
+                                "[@id = '"+modelTranslator.netexId(vehicleJourney)+"']/" +
+                                "netex:ShortName/"+
+                                "text() = '"+vehicleJourney.getPublishedJourneyIdentifier()+"')";
+            assertXPathTrue( xPathExpr);
+        }
+    }
+    
+    @Test(groups = {"TimetableFrame", "vehicleJourneys"}, description = "Validate presence of ServiceJourney with expected description")
+    public void verifyDescription() throws XPathExpressionException, ParseException {
+        
+        List<VehicleJourney> vehicleJourneys = line.getVehicleJourneys();
+        
+        for( VehicleJourney vehicleJourney : vehicleJourneys) {
+           String xPathExpr = "boolean(//netex:TimetableFrame/netex:vehicleJourneys/"+
+                                "netex:ServiceJourney"+
+                                "[@id = '"+modelTranslator.netexId(vehicleJourney)+"']/" +
+                                "netex:Description/"+
+                                "text() = '"+vehicleJourney.getComment()+"')";
+            assertXPathTrue( xPathExpr);
+        }
+    }
+    
+    @Test(groups = {"TimetableFrame", "vehicleJourneys"}, description = "Validate presence of ServiceJourney with expected serviceStatusValue")
+    public void verifyServiceStatusValue() throws XPathExpressionException, ParseException {
+        
+        List<VehicleJourney> vehicleJourneys = line.getVehicleJourneys();
+        
+        for( VehicleJourney vehicleJourney : vehicleJourneys) {
+           String xPathExpr = "boolean(//netex:TimetableFrame/netex:vehicleJourneys/"+
+                                "netex:ServiceJourney"+
+                                "[@id = '"+modelTranslator.netexId(vehicleJourney)+"']/" +
+                                "netex:ServiceAlteration/"+
+                                "text() = '"+vehicleJourney.getServiceStatusValue()+"')";
+            assertXPathTrue( xPathExpr);
+        }
+    }
+    
+    @Test(groups = {"TimetableFrame", "vehicleJourneys"}, description = "Validate presence of ServiceJourney with expected routeRef")
+    public void verifyRouteRef() throws XPathExpressionException, ParseException {
+        
+        List<VehicleJourney> vehicleJourneys = line.getVehicleJourneys();
+        
+        for( VehicleJourney vehicleJourney : vehicleJourneys) {
+           String xPathExpr = "boolean(//netex:TimetableFrame/netex:vehicleJourneys/"+
+                                "netex:ServiceJourney"+
+                                "[@id = '"+modelTranslator.netexId(vehicleJourney)+"']/" +
+                                "netex:RouteRef[ @ref = '"+modelTranslator.netexId(vehicleJourney.getRoute()) +"'])";
+            assertXPathTrue( xPathExpr);
+        }
+    }
+    
+    @Test(groups = {"TimetableFrame", "vehicleJourneys"}, description = "Validate presence of ServiceJourney with expected servicePatternRef")
+    public void verifyServicePatternRef() throws XPathExpressionException, ParseException {
+        
+        List<VehicleJourney> vehicleJourneys = line.getVehicleJourneys();
+        
+        for( VehicleJourney vehicleJourney : vehicleJourneys) {
+           String xPathExpr = "boolean(//netex:TimetableFrame/netex:vehicleJourneys/"+
+                                "netex:ServiceJourney"+
+                                "[@id = '"+modelTranslator.netexId(vehicleJourney)+"']/" +
+                                "netex:ServicePatternRef[ @ref = '"+modelTranslator.netexId(vehicleJourney.getJourneyPattern()) +"'])";
+            assertXPathTrue( xPathExpr);
+        }
+    }
+    
+    @Test(groups = {"TimetableFrame", "vehicleJourneys"}, description = "Validate presence of ServiceJourney with expected operatorId")
+    public void verifyOperatorRef() throws XPathExpressionException, ParseException {
+        
+        List<VehicleJourney> vehicleJourneys = line.getVehicleJourneys();
+        
+        for( VehicleJourney vehicleJourney : vehicleJourneys) {
+           String xPathExpr = "boolean(//netex:TimetableFrame/netex:vehicleJourneys/"+
+                                "netex:ServiceJourney"+
+                                "[@id = '"+modelTranslator.netexId(vehicleJourney)+"']/" +
+                                "netex:OperatorRef[ @ref = '"+modelTranslator.netexId(vehicleJourney.getCompany()) +"'])";
+            assertXPathTrue( xPathExpr);
+        }
+    }
+    
+    @Test(groups = {"TimetableFrame", "dayTypes"}, description = "Validate presence of ServiceJourney with expected DayTypeRef")
+    public void verifyDayTypeRef() throws XPathExpressionException, ParseException {
+        
+        List<VehicleJourney> vehicleJourneys = line.getVehicleJourneys();
+        
+        for( VehicleJourney vehicleJourney : vehicleJourneys) {
+            for ( Timetable timetable : vehicleJourney.getTimetables()) {
+               String xPathExpr = "boolean(//netex:TimetableFrame/netex:vehicleJourneys/"+
+                                    "netex:ServiceJourney"+
+                                    "[@id = '"+modelTranslator.netexId(vehicleJourney)+"']/" +
+                                    "netex:dayTypes/"+
+                                    "netex:DayTypeRef[ @ref = '"+modelTranslator.netexId(timetable) +"'])";
+               assertXPathTrue( xPathExpr);
+            }
         }
     }
     
