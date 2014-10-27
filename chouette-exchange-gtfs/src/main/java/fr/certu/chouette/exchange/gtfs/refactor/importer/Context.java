a081886ad6ea413da7202eae03eb91c30406935a
dsuru5244 <dsuru@cityway.fr>
2014-10-27 17:26:59 +0100

    [DSU] refactor exchange gtfs

---
+++ chouette-exchange-gtfs/src/main/java/fr/certu/chouette/exchange/gtfs/refactor/importer/Context.java
@@ -0,0 +1,42 @@
+package fr.certu.chouette.exchange.gtfs.refactor.importer;
+
+import java.util.HashMap;
+
+public class Context extends HashMap<String, Object>
+{
+   private static final long serialVersionUID = 1L;
+
+   public static final String PATH = "path";
+
+   public static final String ID = "id";
+   public static final String FIELD = "field";
+   public static final String CODE = "code";
+   public static final String VALUE = "value";
+
+   public Context()
+   {
+      super();
+   }
+
+   public Context(String path, int id, String field, GtfsException.ERROR error,
+         Object value)
+   {
+      put(PATH, path);
+      put(ID, id);
+      put(FIELD, field);
+      put(CODE, error);
+      put(VALUE, value);
+
+   }
+
+   public Context(Context context)
+   {
+      put(PATH, context.get(PATH));
+      put(ID, context.get(ID));
+      put(FIELD, context.get(FIELD));
+      put(CODE, context.get(CODE));
+      put(VALUE, context.get(VALUE));
+
+   }
+
+}
