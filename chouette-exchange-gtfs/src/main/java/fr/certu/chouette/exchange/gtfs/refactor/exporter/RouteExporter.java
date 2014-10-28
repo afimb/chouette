9ac160c6409c6dcd7a9afe828dca32cd683606ce
dsuru5244 <dsuru@cityway.fr>
2014-10-28 16:35:32 +0100

    [DSU] refactor exchange gtfs

--- chouette-exchange-gtfs/src/main/java/fr/certu/chouette/exchange/gtfs/refactor/exporter/RouteExporter.java
+++ chouette-exchange-gtfs/src/main/java/fr/certu/chouette/exchange/gtfs/refactor/exporter/RouteExporter.java
@@ -13,7 +13,7 @@ public class RouteExporter extends ExporterImpl<GtfsRoute> implements
 {
    public static enum FIELDS
    {
-      route_id, Route_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color;
+      route_id, agency_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color;
    };
 
    public static final String FILENAME = "routes.txt";
