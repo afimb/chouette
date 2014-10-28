aab5e7cb9c2e71a4bdd08dc07a65e617be7f9bb9
dsuru5244 <dsuru@cityway.fr>
2014-10-28 12:40:25 +0100

    [DSU] refactor exchange gtfs

--- chouette-exchange-gtfs/src/main/java/fr/certu/chouette/exchange/gtfs/refactor/exporter/ExporterImpl.java
+++ chouette-exchange-gtfs/src/main/java/fr/certu/chouette/exchange/gtfs/refactor/exporter/ExporterImpl.java
@@ -2,7 +2,7 @@ package fr.certu.chouette.exchange.gtfs.refactor.exporter;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,7 +17,7 @@ public abstract class ExporterImpl<T> implements Exporter<T>
    public ExporterImpl(String name) throws IOException
    {
       Path path = Paths.get(name);
-      _writer = Files.newBufferedWriter(path, Charset.defaultCharset());
+      _writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8);
       writeHeader();
    }
 
