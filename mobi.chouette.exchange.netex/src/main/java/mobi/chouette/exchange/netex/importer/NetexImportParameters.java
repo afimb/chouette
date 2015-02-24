d972760581c70f1bc5bb3daa74c54a3034a009da
dsuru5244 <dsuru@cityway.fr>
2015-02-24 14:48:23 +0100

    [DSU] 24/01/15

--- mobi.chouette.exchange.netex/src/main/java/mobi/chouette/exchange/netex/importer/NetexImportParameters.java
+++ mobi.chouette.exchange.netex/src/main/java/mobi/chouette/exchange/netex/importer/NetexImportParameters.java
@@ -3,13 +3,13 @@ package mobi.chouette.exchange.netex.importer;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 
-import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import mobi.chouette.exchange.parameters.AbstractImportParameter;
 
-
-@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
 @XmlAccessorType(XmlAccessType.FIELD)
-public class NetexImportParameters  extends AbstractImportParameter{
-
+public class NetexImportParameters extends AbstractImportParameter {
 
 }
