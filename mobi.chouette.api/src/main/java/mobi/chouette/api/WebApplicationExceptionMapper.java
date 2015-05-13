2537863d9a7852ea45ca4050e6ee1898538a3c11
Michel Etienne <metienne@cityway.fr>
2015-05-13 10:42:22 +0200

    return error 404 detail as json

--- mobi.chouette.api/src/main/java/mobi/chouette/api/WebApplicationExceptionMapper.java
+++ mobi.chouette.api/src/main/java/mobi/chouette/api/WebApplicationExceptionMapper.java
@@ -1,14 +1,20 @@
 package mobi.chouette.api;
 
+import java.text.MessageFormat;
+
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
 public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
+	
+	private static final String jsonTemplate = "'{' \"error_code\" : \"{0}\"'}'";
 
 	@Override
 	public Response toResponse(WebApplicationException exception) {
-		return Response.status(exception.getResponse().getStatus()).entity(exception.getMessage()).build();
+		if (exception.getMessage() != null && !exception.getMessage().isEmpty())
+		  return Response.status(exception.getResponse().getStatus()).entity(MessageFormat.format(jsonTemplate, exception.getMessage())).build();
+        return exception.getResponse();
 	}
 
 }
