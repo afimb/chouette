35095b6b40c7a8a33f6e93c21a99e371f967ba24
Michel Etienne <metienne@cityway.fr>
2015-07-01 14:46:55 +0200

    Mantis 37204 : report SQL errors

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/StopAreaRegisterCommand.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/StopAreaRegisterCommand.java
@@ -1,6 +1,7 @@
 package mobi.chouette.exchange.importer;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
@@ -11,6 +12,9 @@ import mobi.chouette.common.Color;
 import mobi.chouette.common.Context;
 import mobi.chouette.common.chain.Command;
 import mobi.chouette.common.chain.CommandFactory;
+import mobi.chouette.exchange.report.ActionError;
+import mobi.chouette.exchange.report.ActionReport;
+import mobi.chouette.exchange.report.LineError;
 import mobi.chouette.model.ConnectionLink;
 import mobi.chouette.model.StopArea;
 import mobi.chouette.model.type.ChouetteAreaEnum;
@@ -29,12 +33,12 @@ public class StopAreaRegisterCommand implements Command {
 	private Predicate<StopArea> predicate = new Predicate<StopArea>() {
 		@Override
 		public boolean apply(StopArea area) {
-			return area.getAreaType().equals(ChouetteAreaEnum.Quay) ||  area.getAreaType().equals(ChouetteAreaEnum.BoardingPosition);
+			return area.getAreaType().equals(ChouetteAreaEnum.Quay)
+					|| area.getAreaType().equals(ChouetteAreaEnum.BoardingPosition);
 		}
 	};
 
 	public static final String COMMAND = "StopAreaRegisterCommand";
-
 
 	@Override
 	public boolean execute(Context context) throws Exception {
@@ -44,32 +48,55 @@ public class StopAreaRegisterCommand implements Command {
 		Command command = CommandFactory.create(initialContext, StopAreaRegisterBlocCommand.class.getName());
 		Command commandLink = CommandFactory.create(initialContext, ConnectionLinkRegisterBlocCommand.class.getName());
 		Monitor monitor = MonitorFactory.start(COMMAND);
-		Referential referential = (Referential) context.get(REFERENTIAL);
-		Collection<StopArea> orderedAreas =  Collections2.filter(referential.getStopAreas().values(), predicate);
-		Iterable<List<StopArea>> iterator = Iterables.partition(orderedAreas, 5000);
-		int count = 0;
-		for (List<StopArea> areas : iterator) {
-			count += areas.size();
-			context.put(AREA_BLOC, areas);
-			command.execute(context);
-			// executeBloc( context, areas);
-			log.info("Areas proceded :"+count+"/"+orderedAreas.size());
+		try {
+			Referential referential = (Referential) context.get(REFERENTIAL);
+			Collection<StopArea> orderedAreas = Collections2.filter(referential.getStopAreas().values(), predicate);
+			Iterable<List<StopArea>> iterator = Iterables.partition(orderedAreas, 30000);
+			int count = 0;
+			for (List<StopArea> areas : iterator) {
+				count += areas.size();
+				context.put(AREA_BLOC, areas);
+				command.execute(context);
+				// executeBloc( context, areas);
+				log.info("Areas proceded :" + count + "/" + orderedAreas.size());
+			}
+			Collection<ConnectionLink> orderedlinks = referential.getConnectionLinks().values();
+			Iterable<List<ConnectionLink>> iterator2 = Iterables.partition(orderedlinks, 30000);
+			count = 0;
+			for (List<ConnectionLink> links : iterator2) {
+				count += links.size();
+				context.put(CONNECTION_LINK_BLOC, links);
+				commandLink.execute(context);
+				// executeBloc( context, areas);
+				log.info("ConnectionLinks proceded :" + count + "/" + orderedlinks.size());
+			}
+		} catch (Exception ex) {
+			ActionReport report = (ActionReport) context.get(REPORT);
+			if (ex.getCause() != null) {
+				Throwable e = ex.getCause();
+				while (e.getCause() != null) {
+					log.error(e.getMessage());
+					e = e.getCause();
+				}
+				if (e instanceof SQLException) {
+					e = ((SQLException) e).getNextException();
+					ActionError error = new ActionError(ActionError.CODE.INTERNAL_ERROR, e.getMessage());
+					report.setFailure(error);
+				} else {
+					ActionError error = new ActionError(ActionError.CODE.INTERNAL_ERROR, e.getMessage());
+					report.setFailure(error);
+				}
+			} else {
+				ActionError error = new ActionError(ActionError.CODE.INTERNAL_ERROR, ex.getMessage());
+				report.setFailure(error);
+			}
+
+		} finally {
+			log.info(Color.MAGENTA + monitor.stop() + Color.NORMAL);
 		}
-		Collection<ConnectionLink> orderedlinks =  referential.getConnectionLinks().values();
-		Iterable<List<ConnectionLink>> iterator2 = Iterables.partition(orderedlinks, 5000);
-		count = 0;
-		for (List<ConnectionLink> links : iterator2) {
-			count += links.size();
-			context.put(CONNECTION_LINK_BLOC, links);
-			commandLink.execute(context);
-			// executeBloc( context, areas);
-			log.info("ConnectionLinks proceded :"+count+"/"+orderedlinks.size());
-		}
-		log.info(Color.MAGENTA + monitor.stop() + Color.NORMAL);
 		return result;
 
 	}
-
 
 	public static class DefaultCommandFactory extends CommandFactory {
 
@@ -82,7 +109,6 @@ public class StopAreaRegisterCommand implements Command {
 	}
 
 	static {
-		CommandFactory.factories.put(StopAreaRegisterCommand.class.getName(),
-				new DefaultCommandFactory());
+		CommandFactory.factories.put(StopAreaRegisterCommand.class.getName(), new DefaultCommandFactory());
 	}
 }
