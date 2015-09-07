521287d644471a96dca76d9b1bc2f3daded1488a
Michel Etienne <metienne@cityway.fr>
2015-09-07 13:32:17 +0200

    add testng for frequencies GTFS validation

---
+++ mobi.chouette.exchange.gtfs/src/test/java/mobi/chouette/exchange/gtfs/validation/Phase2FrequencyTests.java
@@ -0,0 +1,219 @@
+package mobi.chouette.exchange.gtfs.validation;
+
+import lombok.extern.log4j.Log4j;
+import mobi.chouette.common.Color;
+import mobi.chouette.exchange.validation.report.CheckPoint;
+import mobi.chouette.exchange.validation.report.Detail;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+@Log4j
+public class Phase2FrequencyTests extends ValidationTests {
+	
+	
+	@BeforeSuite 
+	public void init()
+	{
+		super.init();
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "missing column trip_id" ,priority=190 )
+	public void verifyTest_2_1_1() throws Exception {
+		log.info(Color.GREEN + "Frequency_1_1 : missing column trip_id" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_1_1", "1-GTFS-Frequency-1",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "missing column start_time" ,priority=191)
+	public void verifyTest_2_1_2() throws Exception {
+		log.info(Color.GREEN + "Frequency_1_2 : missing column start_time" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_1_2", "1-GTFS-Frequency-1",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "missing column end_time" ,priority=192)
+	public void verifyTest_2_1_3() throws Exception {
+		log.info(Color.GREEN + "Frequency_1_3 : missing column end_time" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_1_3", "1-GTFS-Frequency-1",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "missing column headway_secs" ,priority=193)
+	public void verifyTest_2_1_4() throws Exception {
+		log.info(Color.GREEN + "Frequency_1_4 : missing column headway_secs" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_1_4", "1-GTFS-Frequency-1",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "empty column trip_id" ,priority=194 )
+	public void verifyTest_2_2_1() throws Exception {
+		log.info(Color.GREEN + "Frequency_2_1 : empty column trip_id" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_2_1", "1-GTFS-Frequency-2",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "empty column start_time" ,priority=195 )
+	public void verifyTest_2_2_2() throws Exception {
+		log.info(Color.GREEN + "Frequency_2_2 : empty column start_time" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_2_2", "1-GTFS-Frequency-2",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "empty column end_time" ,priority=196 )
+	public void verifyTest_2_2_3() throws Exception {
+		log.info(Color.GREEN + "Frequency_2_3 : empty column end_time" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_2_3", "1-GTFS-Frequency-2",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "empty column headway_secs" ,priority=197 )
+	public void verifyTest_2_2_4() throws Exception {
+		log.info(Color.GREEN + "Frequency_2_4 : empty column headway_secs" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_2_4", "1-GTFS-Frequency-2",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "invalid column start_time" ,priority=198 )
+	public void verifyTest_2_3() throws Exception {
+		log.info(Color.GREEN + "Frequency_3 : invalid column start_time" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_3", "1-GTFS-Frequency-3",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "invalid column end_time" ,priority=199 )
+	public void verifyTest_2_4() throws Exception {
+		log.info(Color.GREEN + "Frequency_4 : invalid column end_time" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_4", "1-GTFS-Frequency-4",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "invalid column headway_secs" ,priority=200 )
+	public void verifyTest_2_5() throws Exception {
+		log.info(Color.GREEN + "Frequency_5 : invalid column headway_secs" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_5", "1-GTFS-Frequency-5",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "invalid column exact_times" ,priority=201 )
+	public void verifyTest_2_6() throws Exception {
+		log.info(Color.GREEN + "Frequency_6 : invalid column wheelchair_accessible" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_6", "1-GTFS-Frequency-6",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Frequency" }, description = "extra columns" ,priority=202 )
+	public void verifyTest_2_7() throws Exception {
+		log.info(Color.GREEN + "Frequency_7 : extra column detected" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "frequency_7", "1-GTFS-Frequency-7",CheckPoint.SEVERITY.WARNING, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "frequencies.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+
+
+}
