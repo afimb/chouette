a7bda8baf810bd6dc385358493c1558e1fe04493
Michel Etienne <metienne@cityway.fr>
2015-09-07 16:51:12 +0200

    add testng for transfer and shape GTFS validation

---
+++ mobi.chouette.exchange.gtfs/src/test/java/mobi/chouette/exchange/gtfs/validation/Phase2ShapeTests.java
@@ -0,0 +1,234 @@
+package mobi.chouette.exchange.gtfs.validation;
+
+import lombok.extern.log4j.Log4j;
+import mobi.chouette.common.Color;
+import mobi.chouette.exchange.validation.report.CheckPoint;
+import mobi.chouette.exchange.validation.report.Detail;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+@Log4j
+public class Phase2ShapeTests extends ValidationTests {
+	
+	
+	@BeforeSuite 
+	public void init()
+	{
+		super.init();
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "missing column shape_id" ,priority=230)
+	public void verifyTest_2_1_1() throws Exception {
+		log.info(Color.GREEN + "Shape_1_1 : missing column shape_id" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_1_1", "1-GTFS-Shape-1",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "missing column shape_pt_lat" ,priority=231)
+	public void verifyTest_2_1_2() throws Exception {
+		log.info(Color.GREEN + "Shape_1_2 : missing column shape_pt_lat" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_1_2", "1-GTFS-Shape-1",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "missing column shape_pt_lon" ,priority=232)
+	public void verifyTest_2_1_3() throws Exception {
+		log.info(Color.GREEN + "Shape_1_3 : missing column shape_pt_lon" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_1_3", "1-GTFS-Shape-1",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "missing column shape_pt_sequence" ,priority=233)
+	public void verifyTest_2_1_4() throws Exception {
+		log.info(Color.GREEN + "Shape_1_4 : missing column shape_pt_sequence" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_1_4", "1-GTFS-Shape-1",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "empty column shape_id" ,priority=234 )
+	public void verifyTest_2_2_1() throws Exception {
+		log.info(Color.GREEN + "Shape_2_1 : empty column shape_id" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_2_1", "1-GTFS-Shape-2",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "empty column shape_pt_lat" ,priority=235 )
+	public void verifyTest_2_2_2() throws Exception {
+		log.info(Color.GREEN + "Shape_2_2 : empty column shape_pt_lat" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_2_2", "1-GTFS-Shape-2",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "empty column shape_pt_lon" ,priority=236 )
+	public void verifyTest_2_2_3() throws Exception {
+		log.info(Color.GREEN + "Shape_2_3 : empty column shape_pt_lon" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_2_3", "1-GTFS-Shape-2",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+		
+	@Test(groups = { "Phase 2 Shape" }, description = "empty column shape_pt_sequence" ,priority=237 )
+	public void verifyTest_2_2_4() throws Exception {
+		log.info(Color.GREEN + "Shape_2_4 : empty column shape_pt_sequence" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_2_4", "1-GTFS-Shape-2",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "duplicate shape_id,shape_pt_sequence" ,priority=238 )
+	public void verifyTest_2_3() throws Exception {
+		log.info(Color.GREEN + "Shape_3 : duplicate shape_id,shape_pt_sequence" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_3", "1-GTFS-Shape-3",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(3), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "invalid column shape_pt_lat" ,priority=239 )
+	public void verifyTest_2_4() throws Exception {
+		log.info(Color.GREEN + "Shape_4 : invalid column shape_pt_lat" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_4", "1-GTFS-Shape-4",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "invalid column shape_pt_lon" ,priority=240 )
+	public void verifyTest_2_5() throws Exception {
+		log.info(Color.GREEN + "Shape_5 : invalid column shape_pt_lon" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_5", "1-GTFS-Shape-5",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "invalid column shape_pt_sequence" ,priority=241 )
+	public void verifyTest_2_6() throws Exception {
+		log.info(Color.GREEN + "Shape_6 : invalid column shape_pt_sequence" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_6", "1-GTFS-Shape-6",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "invalid column shape_dist_traveled" ,priority=242 )
+	public void verifyTest_2_7() throws Exception {
+		log.info(Color.GREEN + "Shape_7 : invalid column shape_dist_traveled" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_7", "1-GTFS-Shape-7",CheckPoint.SEVERITY.ERROR, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(2), "detail must refer bad line");
+		}
+	}
+	
+	@Test(groups = { "Phase 2 Shape" }, description = "extra columns" ,priority=243 )
+	public void verifyTest_2_8() throws Exception {
+		log.info(Color.GREEN + "Shape_8 : extra column detected" + Color.NORMAL);
+		CheckPoint result = verifyValidation( log, "shape_8", "1-GTFS-Shape-8",CheckPoint.SEVERITY.WARNING, CheckPoint.RESULT.NOK,true);
+
+		Assert.assertEquals(result.getDetailCount(), 1, "detail count");
+		for (Detail detail : result.getDetails()) 
+		{
+			Assert.assertNotNull(detail.getSource(), "detail must refer a source");
+			Assert.assertNotNull(detail.getSource().getFile(), "detail must refer a file source");
+			Assert.assertEquals(detail.getSource().getFile().getFilename(), "shapes.txt", "detail must refer bad file");
+			Assert.assertEquals(detail.getSource().getFile().getLineNumber(), Integer.valueOf(1), "detail must refer bad line");
+		}
+	}
+
+
+}
