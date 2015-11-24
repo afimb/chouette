313fb9ddec257ad33bf567e4aa6d18e141495697
Zakaria BOUZIANE <zbouziane@gmail.com>
2015-11-24 12:53:31 +0100

    Journey Frequency

---
+++ mobi.chouette.model/src/main/java/mobi/chouette/model/type/JourneyCategoryEnum.java
@@ -0,0 +1,12 @@
+package mobi.chouette.model.type;
+
+/**
+ * Specifies the Vehicle Journey Categories.
+ * 
+ * @author zbouziane
+ *
+ */
+public enum JourneyCategoryEnum {
+	Timesheet,
+	Frequency;
+}
