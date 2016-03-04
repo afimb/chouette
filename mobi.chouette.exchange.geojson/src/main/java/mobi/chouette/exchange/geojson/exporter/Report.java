a7ba8e359b38329b4eb1e41d95d84908d4c3ff99
dsuru5244 <dsuru@cityway.fr>
2016-03-04 14:39:19 +0100

    [DSU] 04/03/2016

--- mobi.chouette.exchange.geojson/src/main/java/mobi/chouette/exchange/geojson/exporter/Report.java
+++ mobi.chouette.exchange.geojson/src/main/java/mobi/chouette/exchange/geojson/exporter/Report.java
@@ -16,45 +16,47 @@ public class Report implements Constant {
 
 	// global stats
 	public static void addGlobalStats(Context context, DataStats stats) {
-		ActionReport report = (ActionReport) context.get(REPORT);
+		ActionReport report = getReport(context);
 		report.setStats(stats);
 	}
 
 	// line info
 	public static void addLineInfo(Context context, Line line, DataStats stats) {
-		ActionReport report = (ActionReport) context.get(REPORT);
+		ActionReport report = getReport(context);
 		LineInfo lineInfo = getLineInfo(report, line);
 		lineInfo.setStats(stats);
 	}
 
 	public static void addLineInfo(Context context, Line line, LINE_STATE status) {
-		ActionReport report = (ActionReport) context.get(REPORT);
+		ActionReport report = getReport(context);
 		LineInfo lineInfo = getLineInfo(report, line);
 		lineInfo.setStatus(status);
 	}
 
 	public static void addLineInfo(Context context, Line line, LineError error) {
-		ActionReport report = (ActionReport) context.get(REPORT);
+		ActionReport report = getReport(context);
 		LineInfo lineInfo = getLineInfo(report, line);
 		lineInfo.setStatus(LINE_STATE.ERROR);
 		lineInfo.getErrors().add(error);
 	}
 
 	// file info
-	public static void addFileInfo(Context context, String name, FILE_STATE status) {
-		ActionReport report = (ActionReport) context.get(REPORT);
+	public static void addFileInfo(Context context, String name,
+			FILE_STATE status) {
+		ActionReport report = getReport(context);
 		FileInfo lineInfo = getFileInfo(report, name);
 		lineInfo.setStatus(status);
 	}
-	
+
 	public static void addFileInfo(Context context, String name, FileError error) {
-		ActionReport report = (ActionReport) context.get(REPORT);
+		ActionReport report = getReport(context);
 		FileInfo fileInfo = getFileInfo(report, name);
 		fileInfo.setStatus(FILE_STATE.ERROR);
 		fileInfo.getErrors().add(error);
 	}
-	
+
 	// private
+
 	private static LineInfo getLineInfo(ActionReport report, Line line) {
 		LineInfo result = report.findLineInfo(line.getObjectId());
 		if (result == null) {
@@ -68,8 +70,20 @@ public class Report implements Constant {
 	private static FileInfo getFileInfo(ActionReport report, String name) {
 		FileInfo result = report.findFileInfo(name);
 		if (result == null) {
-			result = new FileInfo();
+			result = new FileInfo(name, FILE_STATE.OK);
 			report.getFiles().add(result);
+		}
+		return result;
+	}
+
+	private static ActionReport getReport(Context context) {
+		ActionReport result = null;
+		Object object = context.get(REPORT);
+		if (object != null && object instanceof ActionReport) {
+			result = (ActionReport) object;
+		} else {
+			result = new ActionReport();
+			context.put(REPORT, result);
 		}
 		return result;
 	}
