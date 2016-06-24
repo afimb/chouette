0d9a8dc9f98a66899217d6a5f56f2281c525e81f
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-06-24 13:37:17 +0200

    NRP-453 Add netex java model

---
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/updater/NeTExStopPlaceRegisterUpdater.java
@@ -0,0 +1,7 @@
+package mobi.chouette.exchange.importer.updater;
+
+/**
+ * Created by cristoffer on 23.06.16.
+ */
+public class NeTExStopPlaceRegisterUpdater {
+}
---
+++ mobi.chouette.exchange/src/test/java/mobi/chouette/exchange/importer/updater/NeTExStopPlaceRegisterUpdaterTest.java
@@ -0,0 +1,10 @@
+package mobi.chouette.exchange.importer.updater;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by cristoffer on 23.06.16.
+ */
+public class NeTExStopPlaceRegisterUpdaterTest {
+
+}
