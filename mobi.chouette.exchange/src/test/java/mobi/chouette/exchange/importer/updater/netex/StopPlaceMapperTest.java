0184cb3bbae20aaefe9b35feef1d34218351c7e4
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-08-02 12:41:12 +0200

    NRP-453 Remove test with stop points. Check name of first quay when mapping stop area with three boarding positions

--- mobi.chouette.exchange/src/test/java/mobi/chouette/exchange/importer/updater/netex/StopPlaceMapperTest.java
+++ mobi.chouette.exchange/src/test/java/mobi/chouette/exchange/importer/updater/netex/StopPlaceMapperTest.java
@@ -3,6 +3,7 @@ package mobi.chouette.exchange.importer.updater.netex;
 import mobi.chouette.model.StopArea;
 import mobi.chouette.model.StopPoint;
 import mobi.chouette.model.type.ChouetteAreaEnum;
+import no.rutebanken.netex.model.Quay;
 import no.rutebanken.netex.model.StopPlace;
 import org.junit.Test;
 
@@ -14,27 +15,6 @@ import static org.testng.Assert.*;
 public class StopPlaceMapperTest {
 
     private StopPlaceMapper stopPlaceMapper = new StopPlaceMapper();
-
-    @Test
-    public void stopPlaceWithBoardingPositionsWithStopPoint() {
-        StopArea stopPlace = createStopPlace("Skjeberg");
-
-        StopArea firstBoardingPosition = createBoardingPosition(stopPlace.getName());
-        StopArea secondBoardingPosition = createBoardingPosition(stopPlace.getName());
-        stopPlace.setContainedStopAreas(Arrays.asList(firstBoardingPosition, secondBoardingPosition));
-
-        StopPoint stopPoint = new StopPoint();
-        stopPoint.setContainedInStopArea(firstBoardingPosition);
-        firstBoardingPosition.setContainedStopPoints(Collections.singletonList(stopPoint));
-
-        StopPlace netexStopPlace = stopPlaceMapper.mapStopAreaToStopPlace(stopPlace);
-
-        assertNotNull(netexStopPlace);
-        assertEquals(netexStopPlace.getName().getValue(), stopPlace.getName());
-
-        assertNotNull(netexStopPlace.getQuays(), "Quays shall not be null.");
-        assertEquals(netexStopPlace.getQuays().getQuayRefOrQuay().size(), 2);
-    }
 
     @Test
     public void stopPlaceWithThreeBoardingPositions() {
@@ -52,6 +32,8 @@ public class StopPlaceMapperTest {
 
         assertNotNull(netexStopPlace.getQuays(), "Quays shall not be null.");
         assertEquals(netexStopPlace.getQuays().getQuayRefOrQuay().size(), 3);
+        Quay firstQuay = (Quay) netexStopPlace.getQuays().getQuayRefOrQuay().get(0);
+        assertEquals(firstQuay.getName().getValue(), stopPlace.getName());
     }
 
     @Test
@@ -66,15 +48,12 @@ public class StopPlaceMapperTest {
 
     @Test
     public void boardingPositionWithoutStopPlace() {
-        StopArea boardingPostion = createBoardingPosition("Borgen");
-        StopPlace netexStopPlace = stopPlaceMapper.mapStopAreaToStopPlace(boardingPostion);
+        StopArea boardingPosition = createBoardingPosition("Borgen");
+        StopPlace netexStopPlace = stopPlaceMapper.mapStopAreaToStopPlace(boardingPosition);
 
         assertNotNull(netexStopPlace);
-        assertEquals(netexStopPlace.getName().getValue(), boardingPostion.getName());
+        assertEquals(netexStopPlace.getName().getValue(), boardingPosition.getName());
     }
-
-
-
 
     private StopArea createStopPlace(String name) {
         StopArea stopPlace = new StopArea();
@@ -82,7 +61,6 @@ public class StopPlaceMapperTest {
         stopPlace.setAreaType(ChouetteAreaEnum.StopPlace);
         return stopPlace;
     }
-
 
     private StopArea createBoardingPosition(String name) {
         StopArea boardingPosition = new StopArea();
