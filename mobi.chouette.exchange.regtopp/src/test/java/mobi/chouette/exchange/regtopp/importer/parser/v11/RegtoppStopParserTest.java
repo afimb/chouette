7267f0f9a9ca57b2b9dbcb3365791ad36d75d426
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-08-10 16:50:19 +0200

    NRP-520 Implement (failing) tests for mapping regtopp stop to stop area.

---
+++ mobi.chouette.exchange.regtopp/src/test/java/mobi/chouette/exchange/regtopp/importer/parser/v11/RegtoppStopParserTest.java
@@ -0,0 +1,90 @@
+package mobi.chouette.exchange.regtopp.importer.parser.v11;
+
+import mobi.chouette.exchange.regtopp.importer.RegtoppImportParameters;
+import mobi.chouette.exchange.regtopp.importer.parser.AbstractConverter;
+import mobi.chouette.exchange.regtopp.model.AbstractRegtoppStopHPL;
+import mobi.chouette.exchange.regtopp.model.v11.RegtoppStopHPL;
+import mobi.chouette.model.StopArea;
+import mobi.chouette.model.type.ChouetteAreaEnum;
+import mobi.chouette.model.util.Coordinate;
+import mobi.chouette.model.util.ObjectFactory;
+import mobi.chouette.model.util.Referential;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+import static org.testng.Assert.*;
+
+public class RegtoppStopParserTest {
+
+    private RegtoppStopParser regtoppStopParser = new RegtoppStopParser();
+    private Referential referential = new Referential();
+
+    @Test
+    public void stopAreaShouldBeStopPlaceType() throws Exception {
+        AbstractRegtoppStopHPL regtoppStop = createStop("12345678");
+        RegtoppImportParameters configuration = createConfig("prefix");
+
+        regtoppStopParser.mapRegtoppStop(regtoppStop, configuration, referential, Coordinate.UTM_32N);
+
+        StopArea stopArea = getStopArea(configuration, regtoppStop.getStopId());
+        assertEquals(stopArea.getAreaType(), ChouetteAreaEnum.StopPlace);
+    }
+
+    @Test
+    public void stopAreaShouldContainBoardingPosition() throws Exception {
+        AbstractRegtoppStopHPL regtoppStop = createStop("12345678");
+        RegtoppImportParameters configuration = createConfig("prefix");
+
+        regtoppStopParser.mapRegtoppStop(regtoppStop, configuration, referential, Coordinate.UTM_32N);
+
+        StopArea stopArea = getStopArea(configuration, regtoppStop.getStopId());
+        assertEquals(stopArea.getContainedStopAreas().size(), 1);
+        assertEquals(stopArea.getContainedStopAreas().get(0).getAreaType(), ChouetteAreaEnum.BoardingPosition);
+    }
+
+    @Test
+    public void stopAreaShouldHaveEightDigitsId() throws Exception {
+        AbstractRegtoppStopHPL regtoppStop = createStop("12345678");
+        RegtoppImportParameters configuration = createConfig("prefix");
+
+        regtoppStopParser.mapRegtoppStop(regtoppStop, configuration, referential, Coordinate.UTM_32N);
+
+        StopArea stopArea = getStopArea(configuration, regtoppStop.getStopId());
+        StopArea boardingPosition = stopArea.getContainedStopAreas().get(0);
+
+        assertEquals(boardingPosition.objectIdSuffix().length(), 8);
+    }
+
+    @Test
+    public void boardingPositionShouldHaveTenDigitsId() throws Exception {
+        AbstractRegtoppStopHPL regtoppStop = createStop("11020517");
+        RegtoppImportParameters configuration = createConfig("prefix");
+
+        regtoppStopParser.mapRegtoppStop(regtoppStop, configuration, referential, Coordinate.UTM_32N);
+
+        StopArea stopArea = getStopArea(configuration, regtoppStop.getStopId());
+        assertEquals(stopArea.objectIdSuffix().length(), 10, stopArea.getObjectId());
+    }
+
+    private RegtoppImportParameters createConfig(String prefix) {
+        RegtoppImportParameters configuration = new RegtoppImportParameters();
+        configuration.setObjectIdPrefix(prefix);
+        return configuration;
+    }
+
+
+    private StopArea getStopArea(RegtoppImportParameters configuration, String stopId) {
+        String objectId = AbstractConverter.composeObjectId(configuration.getObjectIdPrefix(), StopArea.STOPAREA_KEY, stopId);
+        return ObjectFactory.getStopArea(referential, objectId);
+    }
+
+    private AbstractRegtoppStopHPL createStop(String stopId) {
+        AbstractRegtoppStopHPL regtoppStop = new RegtoppStopHPL(0, 0, 0);
+        regtoppStop.setStopId(stopId);
+        regtoppStop.setY(new BigDecimal("6533687"));
+        regtoppStop.setX(new BigDecimal("318578"));
+        return regtoppStop;
+    }
+
+}
