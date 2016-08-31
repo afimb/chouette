b15f295b5d58c471ead4a616c0fe7cbd1d2a68ac
Cristoffer Solem <cristoffer.solem@gmail.com>
2016-08-31 14:55:09 +0200

    NRP-520 Avoid using the v11 regtopp stop parser for v12novus parsing, as v11 has changed to generate boarding positions with parent stop places

--- mobi.chouette.exchange.regtopp/src/main/java/mobi/chouette/exchange/regtopp/importer/parser/v12novus/RegtoppStopParser.java
+++ mobi.chouette.exchange.regtopp/src/main/java/mobi/chouette/exchange/regtopp/importer/parser/v12novus/RegtoppStopParser.java
@@ -1,6 +1,7 @@
 package mobi.chouette.exchange.regtopp.importer.parser.v12novus;
 
 import static mobi.chouette.common.Constant.CONFIGURATION;
+import static mobi.chouette.common.Constant.PARSER;
 import static mobi.chouette.common.Constant.REFERENTIAL;
 
 import java.math.BigDecimal;
@@ -14,31 +15,33 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.PrecisionModel;
+import lombok.extern.log4j.Log4j;
 import mobi.chouette.common.Context;
 import mobi.chouette.exchange.importer.Parser;
 import mobi.chouette.exchange.importer.ParserFactory;
 import mobi.chouette.exchange.regtopp.importer.RegtoppImportParameters;
+import mobi.chouette.exchange.regtopp.importer.RegtoppImporter;
 import mobi.chouette.exchange.regtopp.importer.parser.CentroidGenerator;
 import mobi.chouette.exchange.regtopp.importer.parser.ObjectIdCreator;
+import mobi.chouette.exchange.regtopp.model.AbstractRegtoppStopHPL;
 import mobi.chouette.model.StopArea;
 import mobi.chouette.model.type.ChouetteAreaEnum;
 import mobi.chouette.model.type.LongLatTypeEnum;
 import mobi.chouette.model.util.ObjectFactory;
 import mobi.chouette.model.util.Referential;
 
-
+@Log4j
 public class RegtoppStopParser extends mobi.chouette.exchange.regtopp.importer.parser.v11.RegtoppStopParser {
 
-	private CentroidGenerator centroidGenerator = new CentroidGenerator();
+	private final CentroidGenerator centroidGenerator = new CentroidGenerator();
 
 	@Override
 	public void parse(Context context) throws Exception {
-		// Parse as v1.1D
-		super.parse(context);
-
+		RegtoppImporter importer = (RegtoppImporter) context.get(PARSER);
 		Referential referential = (Referential) context.get(REFERENTIAL);
 		RegtoppImportParameters configuration = (RegtoppImportParameters) context.get(CONFIGURATION);
 
+		createBoardingPositions(importer, referential, configuration);
 
 		// 1.2Novus specific
 
@@ -73,6 +76,22 @@ public class RegtoppStopParser extends mobi.chouette.exchange.regtopp.importer.p
 			}
 
 		}
+	}
+
+	private void createBoardingPositions(RegtoppImporter importer, Referential referential, RegtoppImportParameters configuration) throws Exception {
+		for (AbstractRegtoppStopHPL stop : importer.getStopById()) {
+			createBoardingPosition(stop, configuration, referential);
+		}
+	}
+
+	public void createBoardingPosition(AbstractRegtoppStopHPL stop,
+									   RegtoppImportParameters regtoppImportParameters, Referential referential) {
+		String objectId = ObjectIdCreator.createStopAreaId(regtoppImportParameters, stop.getFullStopId());
+
+		StopArea stopArea = ObjectFactory.getStopArea(referential, objectId);
+		stopArea.setName(stop.getFullName());
+		stopArea.setAreaType(ChouetteAreaEnum.BoardingPosition);
+		convertAndSetCoordinates(stopArea, stop.getX(), stop.getY(), regtoppImportParameters.getCoordinateProjection());
 	}
 
 	static {
