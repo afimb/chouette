20fbdc3318fddd9f683c64182937078c7c96d134
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-10-05 12:20:15 +0200

    Revert "NRP-433 Add support for exporting AirService. Whether to use route type extensions or not should be configurable."
    This reverts commit 4621176b642f47c1d5deb86905065f964642e67a.

--- mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/exporter/producer/GtfsRouteProducer.java
+++ mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/exporter/producer/GtfsRouteProducer.java
@@ -107,9 +107,6 @@ public class GtfsRouteProducer extends AbstractProducer
          case Ferry:
             route.setRouteType(RouteTypeEnum.Ferry);
             break;
-         case Air:
-            route.setRouteType(RouteTypeEnum.AirService);
-            break;
          default:
             route.setRouteType(RouteTypeEnum.Bus);
          }
