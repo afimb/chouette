11625b1c9bb71ec476c171548c8e349e4436ea72
Arne Seime <arne.seime@greenbird.com>
2016-10-27 10:39:17 +0200

    Perform mapping even if all stopareas are already converted by stop place register

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/updater/netex/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Temporary package for code related to posting stop places to the stop place register and mapping between chouette model and netex model.
- */
-package mobi.chouette.exchange.importer.updater.netex;
