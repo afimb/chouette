8c43f1298dac8a871a420e4eddbe31bfd0642959
Tommy Gagnes <tommy.gagnes@knowit.no>
2016-12-02 21:35:17 +0100

    Use HTTP POST when cleaning referential

--- mobi.chouette.ws/src/main/java/mobi/chouette/ws/RestService.java
+++ mobi.chouette.ws/src/main/java/mobi/chouette/ws/RestService.java
@@ -1,5 +1,27 @@
 package mobi.chouette.ws;
 
+import lombok.extern.log4j.Log4j;
+import mobi.chouette.common.Color;
+import mobi.chouette.common.Constant;
+import mobi.chouette.common.chain.Command;
+import mobi.chouette.common.chain.CommandFactory;
+import mobi.chouette.exchange.importer.CleanRepositoryCommand;
+import mobi.chouette.model.iev.Job;
+import mobi.chouette.model.iev.Job.STATUS;
+import mobi.chouette.model.iev.Link;
+import mobi.chouette.persistence.hibernate.ContextHolder;
+import mobi.chouette.service.*;
+import org.apache.commons.io.FilenameUtils;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.naming.InitialContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
 import java.io.File;
 import java.io.InputStream;
 import java.net.URI;
@@ -11,50 +33,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.naming.InitialContext;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.CacheControl;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.UriInfo;
-
-import lombok.extern.log4j.Log4j;
-import mobi.chouette.common.Color;
-import mobi.chouette.common.Constant;
-import mobi.chouette.common.chain.Command;
-import mobi.chouette.common.chain.CommandFactory;
-import mobi.chouette.exchange.importer.CleanRepositoryCommand;
-import mobi.chouette.model.iev.Job;
-import mobi.chouette.model.iev.Job.STATUS;
-import mobi.chouette.model.iev.Link;
-import mobi.chouette.persistence.hibernate.ContextHolder;
-import mobi.chouette.service.JobService;
-import mobi.chouette.service.JobServiceManager;
-import mobi.chouette.service.RequestExceptionCode;
-import mobi.chouette.service.RequestServiceException;
-import mobi.chouette.service.ServiceException;
-import mobi.chouette.service.ServiceExceptionCode;
-
-import org.apache.commons.io.FilenameUtils;
-import org.jboss.resteasy.plugins.providers.multipart.InputPart;
-import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 @Path("/referentials")
 @Log4j
@@ -210,8 +188,8 @@ public class RestService implements Constant {
 	}
 
 
-	@DELETE
-	@Path("/{ref}")
+	@POST
+	@Path("/{ref}/clean")
 	public Response clean(@PathParam("ref") String referential) {
 		log.info(Color.CYAN + "Call clean referential = " + referential + Color.NORMAL);
 		try {
