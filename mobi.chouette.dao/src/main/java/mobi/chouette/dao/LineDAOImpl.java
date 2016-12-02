0c5591e7f0da481b3b88aed69b057ff6bcd6db4d
Arne Seime <arne.seime@greenbird.com>
2016-12-02 16:12:15 +0100

    Revert "Added faster delete method for Lines"
    This reverts commit df98adf4e9c049d2aed89b9a05b48cc715cd6cb3.

--- mobi.chouette.dao/src/main/java/mobi/chouette/dao/LineDAOImpl.java
+++ mobi.chouette.dao/src/main/java/mobi/chouette/dao/LineDAOImpl.java
@@ -1,19 +1,14 @@
 package mobi.chouette.dao;
 
-import java.util.Collection;
-
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 import mobi.chouette.model.Line;
 
 @Stateless (name="LineDAO")
 public class LineDAOImpl extends GenericDAOImpl<Line> implements LineDAO {
 
-	private EntityManager deleteEntityManager;
-	
 	public LineDAOImpl() {
 		super(Line.class);
 	}
@@ -23,18 +18,4 @@ public class LineDAOImpl extends GenericDAOImpl<Line> implements LineDAO {
 		this.em = em;
 	}
 	
-	@PersistenceContext(unitName = "referential_delete")
-	public void setDeleteEntityManager(EntityManager em) {
-		this.deleteEntityManager = em;
-	}
-	
-	public void deleteByObjectId(Collection<String> objectIds) {
-		if(objectIds == null || objectIds.size() == 0) {
-			return;
-		}
-		Query deleteQuery = deleteEntityManager.createQuery("DELETE FROM Line where objectId IN :objectIds");
-		deleteQuery.setParameter("objectIds", objectIds);
-		deleteQuery.executeUpdate();
-	}
-
 }
