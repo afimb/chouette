35a8e3fea7a8076f3934214af3298200b3a79a8c
Mathieu Hicauber <mhicauber@okina.fr>
2016-12-23 10:07:09 +0100

    Fix json unmarshalling

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/report/FileError.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/report/FileError.java
@@ -1,31 +1,31 @@
 package mobi.chouette.exchange.report;
 
-import java.io.PrintStream;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import mobi.chouette.exchange.report.ActionReporter.FILE_ERROR_CODE;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import mobi.chouette.exchange.report.ActionReporter.FILE_ERROR_CODE;
-
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
+import java.io.PrintStream;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(propOrder={"code","description"})
+@XmlType(propOrder = {"code", "description"})
 @AllArgsConstructor
 @Data
-@EqualsAndHashCode(callSuper=false)
-public class FileError extends AbstractReport{
-	
-	@XmlElement(name="code",required=true)
+@EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+public class FileError extends AbstractReport {
+
+	@XmlElement(name = "code", required = true)
 	private FILE_ERROR_CODE code;
-	
-	@XmlElement(name="description",required=true)
+
+	@XmlElement(name = "description", required = true)
 	private String description;
 
 	public JSONObject toJson() throws JSONException {
@@ -36,13 +36,13 @@ public class FileError extends AbstractReport{
 	}
 
 	@Override
-	public void print(PrintStream out, StringBuilder ret , int level, boolean first) {
+	public void print(PrintStream out, StringBuilder ret, int level, boolean first) {
 		ret.setLength(0);
 		out.print(addLevel(ret, level).append('{'));
-		out.print(toJsonString(ret, level+1, "code", code, true));
-		out.print(toJsonString(ret, level+1, "description", description, false));
+		out.print(toJsonString(ret, level + 1, "code", code, true));
+		out.print(toJsonString(ret, level + 1, "description", description, false));
 		ret.setLength(0);
-		out.print(addLevel(ret.append('\n'), level).append('}'));		
+		out.print(addLevel(ret.append('\n'), level).append('}'));
 	}
-	
+
 }
