69f38155886c139f788fd0fc66bb8f635b04c42e
Samuel Wirzen <samuel.wirzen@webstep.no>
2017-02-06 09:23:14 +0100

    NRP-635 - Changed setting of version attribute in export

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/AbstractJaxbNetexProducer.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/AbstractJaxbNetexProducer.java
@@ -13,7 +13,7 @@ import java.util.Date;
 
 public abstract class AbstractJaxbNetexProducer<T extends DataManagedObjectStructure, U extends NeptuneIdentifiedObject> {
 
-    public static final String NETEX_DATA_OJBECT_VERSION = "1";
+    public static final String NETEX_DATA_OJBECT_VERSION = "0";
     public static final ZoneId LOCAL_ZONE_ID = ZoneId.of("Europe/Oslo");
     public static final String DEFAULT_ZONE_ID = "UTC";
 
@@ -32,12 +32,7 @@ public abstract class AbstractJaxbNetexProducer<T extends DataManagedObjectStruc
 
     public void populateFromModel(T target, U source) {
         target.setId(source.getObjectId());
-
-        if (source.getObjectVersion() > 0) {
-            target.setVersion(String.valueOf(source.getObjectVersion()));
-        } else {
-            target.setVersion(NETEX_DATA_OJBECT_VERSION);
-        }
+        target.setVersion(source.getObjectVersion() > 0 ? String.valueOf(source.getObjectVersion()) : NETEX_DATA_OJBECT_VERSION);
 
         // TODO make this a parameter option
 /*
