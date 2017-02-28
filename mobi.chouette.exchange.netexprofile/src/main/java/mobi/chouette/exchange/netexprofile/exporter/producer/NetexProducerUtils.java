120f082c018dabd10934e7ffc0687b08c7292c6b
Samuel Wirzén <samuel.wirzen@webstep.no>
2017-02-28 16:59:12 +0100

    NRP-1198 - Added dayTypes-section to calendar frame, NeTEx export

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/NetexProducerUtils.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/NetexProducerUtils.java
@@ -1,10 +1,15 @@
 package mobi.chouette.exchange.netexprofile.exporter.producer;
 
 import lombok.extern.log4j.Log4j;
+import mobi.chouette.model.type.DayTypeEnum;
 import org.rutebanken.netex.model.AllVehicleModesOfTransportEnumeration;
+import org.rutebanken.netex.model.DayOfWeekEnumeration;
 
 import java.sql.Time;
 import java.time.*;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Log4j
@@ -89,6 +94,95 @@ public class NetexProducerUtils {
             return AllVehicleModesOfTransportEnumeration.UNKNOWN;
         else
             return AllVehicleModesOfTransportEnumeration.UNKNOWN;
+    }
+
+    public static List<DayTypeEnum> convertDayOfWeek(DayOfWeekEnumeration dayOfWeek) {
+        List<DayTypeEnum> days = new ArrayList<>();
+
+        switch (dayOfWeek) {
+            case MONDAY:
+                days.add(DayTypeEnum.Monday);
+                break;
+            case TUESDAY:
+                days.add(DayTypeEnum.Tuesday);
+                break;
+            case WEDNESDAY:
+                days.add(DayTypeEnum.Wednesday);
+                break;
+            case THURSDAY:
+                days.add(DayTypeEnum.Thursday);
+                break;
+            case FRIDAY:
+                days.add(DayTypeEnum.Friday);
+                break;
+            case SATURDAY:
+                days.add(DayTypeEnum.Saturday);
+                break;
+            case SUNDAY:
+                days.add(DayTypeEnum.Sunday);
+                break;
+            case EVERYDAY:
+                days.add(DayTypeEnum.Monday);
+                days.add(DayTypeEnum.Tuesday);
+                days.add(DayTypeEnum.Wednesday);
+                days.add(DayTypeEnum.Thursday);
+                days.add(DayTypeEnum.Friday);
+                days.add(DayTypeEnum.Saturday);
+                days.add(DayTypeEnum.Sunday);
+                break;
+            case WEEKDAYS:
+                days.add(DayTypeEnum.Monday);
+                days.add(DayTypeEnum.Tuesday);
+                days.add(DayTypeEnum.Wednesday);
+                days.add(DayTypeEnum.Thursday);
+                days.add(DayTypeEnum.Friday);
+                break;
+            case WEEKEND:
+                days.add(DayTypeEnum.Saturday);
+                days.add(DayTypeEnum.Sunday);
+                break;
+            case NONE:
+                // None
+                break;
+        }
+        return days;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static DayOfWeekEnumeration toDayOfWeekEnumeration(List<DayTypeEnum> dayTypeEnums) {
+        EnumSet weekend = EnumSet.of(DayTypeEnum.Saturday, DayTypeEnum.Sunday);
+        EnumSet weekdays = EnumSet.of(DayTypeEnum.Monday, DayTypeEnum.Tuesday, DayTypeEnum.Wednesday, DayTypeEnum.Thursday, DayTypeEnum.Friday);
+        EnumSet everyday = EnumSet.of(DayTypeEnum.Monday, DayTypeEnum.Tuesday, DayTypeEnum.Wednesday, DayTypeEnum.Thursday, DayTypeEnum.Friday, DayTypeEnum.Saturday, DayTypeEnum.Sunday);
+        EnumSet actualDaysOfWeek = EnumSet.noneOf(DayTypeEnum.class);
+
+        for (DayTypeEnum dayTypeEnum : dayTypeEnums) {
+            actualDaysOfWeek.add(dayTypeEnum);
+        }
+        if (actualDaysOfWeek.isEmpty()) {
+            return DayOfWeekEnumeration.NONE;
+        } else if (actualDaysOfWeek.equals(EnumSet.of(DayTypeEnum.Monday))) {
+            return DayOfWeekEnumeration.MONDAY;
+        } else if (actualDaysOfWeek.equals(EnumSet.of(DayTypeEnum.Tuesday))) {
+            return DayOfWeekEnumeration.TUESDAY;
+        } else if (actualDaysOfWeek.equals(EnumSet.of(DayTypeEnum.Wednesday))) {
+            return DayOfWeekEnumeration.WEDNESDAY;
+        } else if (actualDaysOfWeek.equals(EnumSet.of(DayTypeEnum.Thursday))) {
+            return DayOfWeekEnumeration.THURSDAY;
+        } else if (actualDaysOfWeek.equals(EnumSet.of(DayTypeEnum.Friday))) {
+            return DayOfWeekEnumeration.FRIDAY;
+        } else if (actualDaysOfWeek.equals(EnumSet.of(DayTypeEnum.Saturday))) {
+            return DayOfWeekEnumeration.SATURDAY;
+        } else if (actualDaysOfWeek.equals(EnumSet.of(DayTypeEnum.Sunday))) {
+            return DayOfWeekEnumeration.SUNDAY;
+        } else if (actualDaysOfWeek.equals(weekdays)) {
+            return DayOfWeekEnumeration.WEEKDAYS;
+        } else if (actualDaysOfWeek.equals(weekend)) {
+            return DayOfWeekEnumeration.WEEKEND;
+        } else if (actualDaysOfWeek.equals(everyday)) {
+            return DayOfWeekEnumeration.EVERYDAY;
+        }
+
+        return null;
     }
 
 }
--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/ServiceCalendarFrameProducer.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/ServiceCalendarFrameProducer.java
@@ -2,48 +2,80 @@ package mobi.chouette.exchange.netexprofile.exporter.producer;
 
 import mobi.chouette.exchange.netexprofile.exporter.ExportableData;
 import mobi.chouette.model.Line;
+import mobi.chouette.model.Period;
 import mobi.chouette.model.Timetable;
-import org.rutebanken.netex.model.ServiceCalendar;
-import org.rutebanken.netex.model.ServiceCalendarFrame;
+import mobi.chouette.model.type.DayTypeEnum;
+import org.apache.commons.collections.CollectionUtils;
+import org.rutebanken.netex.model.*;
 
-import java.util.ArrayList;
+import java.sql.Date;
 import java.util.List;
-import java.util.Set;
 
+import static mobi.chouette.exchange.netexprofile.exporter.producer.NetexProducerUtils.isSet;
+import static mobi.chouette.exchange.netexprofile.util.NetexObjectIdTypes.DAY_TYPE_KEY;
 import static mobi.chouette.exchange.netexprofile.util.NetexObjectIdTypes.SERVICE_CALENDAR_FRAME_KEY;
-import static mobi.chouette.exchange.netexprofile.util.NetexObjectIdTypes.SERVICE_CALENDAR_KEY;
 
 public class ServiceCalendarFrameProducer extends NetexProducer { //implements NetexFrameProducer<ServiceCalendarFrame> {
 
     //@Override
-    public List<ServiceCalendarFrame> produce(ExportableData data) {
+    public ServiceCalendarFrame produce(ExportableData data) {
         Line line = data.getLine();
-        Set<Timetable> timetables = data.getTimetables();
-        List<ServiceCalendarFrame> serviceCalendarFrames = new ArrayList<>();
 
-        for (Timetable timetable : timetables) {
+        String serviceCalendarFrameId = netexId(line.objectIdPrefix(), SERVICE_CALENDAR_FRAME_KEY, line.objectIdSuffix());
+
+        ServiceCalendarFrame serviceCalendarFrame = netexFactory.createServiceCalendarFrame()
+                .withVersion("any")
+                .withId(serviceCalendarFrameId);
+
+        DayTypesInFrame_RelStructure dayTypesStruct = netexFactory.createDayTypesInFrame_RelStructure();
+
+        for (Timetable timetable : data.getTimetables()) {
             timetable.computeLimitOfPeriods();
 
-            String serviceCalendarFrameId = netexId(line.objectIdPrefix(), SERVICE_CALENDAR_FRAME_KEY, line.objectIdSuffix());
+            DayType dayType = netexFactory.createDayType();
+            dayType.setVersion(timetable.getObjectVersion() > 0 ? String.valueOf(timetable.getObjectVersion()) : NETEX_DATA_OJBECT_VERSION);
 
-            ServiceCalendarFrame serviceCalendarFrame = netexFactory.createServiceCalendarFrame()
-                    .withVersion("any")
-                    .withId(serviceCalendarFrameId);
+            String dayTypeId = netexId(timetable.objectIdPrefix(), DAY_TYPE_KEY, timetable.objectIdSuffix());
+            dayType.setId(dayTypeId);
 
-            ServiceCalendar serviceCalendar = netexFactory.createServiceCalendar();
-            serviceCalendar.setVersion(timetable.getObjectVersion() > 0 ? String.valueOf(timetable.getObjectVersion()) : NETEX_DATA_OJBECT_VERSION);
+            if (isSet(timetable.getComment())) {
+                dayType.setShortName(getMultilingualString(timetable.getComment()));
+            }
 
-            String serviceCalendarId = netexId(timetable.objectIdPrefix(), SERVICE_CALENDAR_KEY, timetable.objectIdSuffix());
-            serviceCalendar.setId(serviceCalendarId);
+            if (CollectionUtils.isNotEmpty(timetable.getDayTypes())) {
+                List<DayTypeEnum> dayTypeEnums = timetable.getDayTypes();
+                DayOfWeekEnumeration dayOfWeekEnumeration = NetexProducerUtils.toDayOfWeekEnumeration(dayTypeEnums);
 
-            serviceCalendar.setFromDate(NetexProducerUtils.toOffsetDateTime(timetable.getStartOfPeriod()));
-            serviceCalendar.setToDate(NetexProducerUtils.toOffsetDateTime(timetable.getEndOfPeriod()));
+                PropertyOfDay propertyOfDay = netexFactory.createPropertyOfDay();
+                propertyOfDay.getDaysOfWeek().add(dayOfWeekEnumeration);
 
-            serviceCalendarFrame.setServiceCalendar(serviceCalendar);
+                PropertiesOfDay_RelStructure propertiesOfDay = netexFactory.createPropertiesOfDay_RelStructure();
+                propertiesOfDay.getPropertyOfDay().add(propertyOfDay);
 
-            serviceCalendarFrames.add(serviceCalendarFrame);
+                dayType.setProperties(propertiesOfDay);
+            }
+
+            dayTypesStruct.getDayType_().add(netexFactory.createDayType(dayType));
+
+            List<Date> peculiarDates = timetable.getEffectiveDates();
+            for (Date peculiarDate : peculiarDates) {
+                // TODO create operating days?
+            }
+
+            List<Period> effectivePeriods = timetable.getEffectivePeriods();
+            for (Period period : effectivePeriods) {
+                // TODO create operating periods?
+            }
+
+            for (Period period : effectivePeriods) {
+                // TODO create assignments between day types and periods
+            }
+
+            for (Date day : peculiarDates) {
+                // TODO create assignments between day types and operating days
+            }
         }
 
-        return serviceCalendarFrames;
+        return serviceCalendarFrame;
     }
 }
