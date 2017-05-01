ccfaa13e401c754fe45ff8c2a83c5fc5dd3efc4d
Arne Seime <arne.seime@greenbird.com>
2017-05-01 11:03:03 +0200

    add magic parameter to make nsr mapping file retrieving faster

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/validation/norway/StopReferentialIdValidator.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/validation/norway/StopReferentialIdValidator.java
@@ -83,8 +83,8 @@ public class StopReferentialIdValidator implements ExternalReferenceValidator {
 			ExternalReferenceValidator instance = (ExternalReferenceValidator) context.get(NAME);
 			if (instance == null) {
 				// TODO Fetch urls from iev.properties
-				instance = new StopReferentialIdValidator("https://api-test.rutebanken.org/tiamat/1.0/quay/id_mapping",
-						"https://api-test.rutebanken.org/tiamat/1.0/stop_place/id_mapping");
+				instance = new StopReferentialIdValidator("https://api-test.rutebanken.org/tiamat/1.0/quay/id_mapping?recordsPerRoundTrip=220000",
+						"https://api-test.rutebanken.org/tiamat/1.0/stop_place/id_mapping?recordsPerRoundTrip=220000");
 
 				context.put(NAME, instance);
 			}
