555dd903781bd7fed83e4b7d9f9c09597c34cfca
Arne Seime <arne.seime@greenbird.com>
2017-05-05 10:49:28 +0200

    Replace StopArea with Quay on netex updater

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/updater/NeTExStopPlaceRegisterUpdater.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/updater/NeTExStopPlaceRegisterUpdater.java
@@ -173,7 +173,22 @@ public class NeTExStopPlaceRegisterUpdater {
 					// Only replace IDs if ID already contains Chouette ID key (StopArea)
 					stopPlaceMapper.replaceIdIfQuayOrStopPlace(stopPlace);
 				}
-
+				
+				Quays_RelStructure quays = stopPlace.getQuays();
+				if(quays != null && quays.getQuayRefOrQuay() != null) {
+					for (Object q : quays.getQuayRefOrQuay()) {
+						if(q instanceof Quay) {
+							Quay quay= (Quay) q;
+							String qId = quay.getId();
+							if(qId.contains(ObjectIdTypes.STOPAREA_KEY)) {
+								// Only replace IDs if ID already contains Chouette ID key (StopArea)
+								stopPlaceMapper.replaceIdIfQuayOrStopPlace(quay);
+							}
+						}
+					}
+				}
+				
+				
 				if (stopArea == null) {
 					log.error("Could not find StopArea for objectId=" + ToStringBuilder.reflectionToString(stopPlace)
 							+ " correlationId: " + correlationId);
