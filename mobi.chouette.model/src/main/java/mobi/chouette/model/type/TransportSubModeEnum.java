229484e8374e91ed0238b1b3b40103bd417756aa
Samuel Wirzén <samuel.wirzen@webstep.no>
2017-06-07 16:12:15 +0200

    NRP-1611 - Added grouped EnumSet constants for transport sub modes

--- mobi.chouette.model/src/main/java/mobi/chouette/model/type/TransportSubModeEnum.java
+++ mobi.chouette.model/src/main/java/mobi/chouette/model/type/TransportSubModeEnum.java
@@ -1,5 +1,7 @@
 package mobi.chouette.model.type;
 
+import java.util.EnumSet;
+
 public enum TransportSubModeEnum {
 
     /**
@@ -63,6 +65,50 @@ public enum TransportSubModeEnum {
     /**
      * Funicular sub modes
      */
-    Funicular
+    Funicular;
+
+    public static final EnumSet<TransportSubModeEnum> CABELWAY_SUB_MODES = EnumSet.of(TransportSubModeEnum.Telecabin);
+    public static final EnumSet<TransportSubModeEnum> FUNICULAR_SUB_MODES = EnumSet.of(TransportSubModeEnum.Funicular);
+    public static final EnumSet<TransportSubModeEnum> METRO_SUB_MODES = EnumSet.of(TransportSubModeEnum.Metro);
+    public static final EnumSet<TransportSubModeEnum> TRAM_SUB_MODES = EnumSet.of(TransportSubModeEnum.LocalTram);
+
+    public static final EnumSet<TransportSubModeEnum> AIR_SUB_MODES = EnumSet.of(
+            TransportSubModeEnum.DomesticFlight,
+            TransportSubModeEnum.HelicopterService,
+            TransportSubModeEnum.InternationalFlight
+    );
+
+    public static final EnumSet<TransportSubModeEnum> RAIL_SUB_MODES = EnumSet.of(
+            TransportSubModeEnum.International,
+            TransportSubModeEnum.InterregionalRail,
+            TransportSubModeEnum.Local,
+            TransportSubModeEnum.LongDistance,
+            TransportSubModeEnum.NightRail,
+            TransportSubModeEnum.RegionalRail,
+            TransportSubModeEnum.TouristRailway
+    );
+
+    public static final EnumSet<TransportSubModeEnum> WATER_SUB_MODES = EnumSet.of(
+            TransportSubModeEnum.HighSpeedPassengerService,
+            TransportSubModeEnum.HighSpeedVehicleService,
+            TransportSubModeEnum.InternationalCarFerry,
+            TransportSubModeEnum.InternationalPassengerFerry,
+            TransportSubModeEnum.LocalCarFerry,
+            TransportSubModeEnum.LocalPassengerFerry,
+            TransportSubModeEnum.NationalCarFerry,
+            TransportSubModeEnum.SightseeingService
+    );
+
+    public static final EnumSet<TransportSubModeEnum> BUS_SUB_MODES = EnumSet.of(
+            TransportSubModeEnum.AirportLinkBus,
+            TransportSubModeEnum.ExpressBus,
+            TransportSubModeEnum.LocalBus,
+            TransportSubModeEnum.NightBus,
+            TransportSubModeEnum.RailReplacementBus,
+            TransportSubModeEnum.RegionalBus,
+            TransportSubModeEnum.SchoolBus,
+            TransportSubModeEnum.ShuttleBus,
+            TransportSubModeEnum.SightseeingBus
+    );
 
 }
