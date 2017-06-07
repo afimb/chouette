faaa9804fb0c4a368f34973d8ba97c53cbe3a833
Samuel Wirzén <samuel.wirzen@webstep.no>
2017-06-07 15:07:46 +0200

    NRP-1611 - Extended StopArea model with fields for stop area type, transport mode, and transport sub mode

---
+++ mobi.chouette.model/src/main/java/mobi/chouette/model/type/TransportSubModeEnum.java
@@ -0,0 +1,68 @@
+package mobi.chouette.model.type;
+
+public enum TransportSubModeEnum {
+
+    /**
+     * Bus sub modes
+     */
+    AirportLinkBus,
+    ExpressBus,
+    LocalBus,
+    NightBus,
+    RailReplacementBus,
+    RegionalBus,
+    SchoolBus,
+    ShuttleBus,
+    SightseeingBus,
+
+    /**
+     * Tram sub modes
+     */
+    LocalTram,
+
+    /**
+     * Rail sub modes
+     */
+    International,
+    InterregionalRail,
+    Local,
+    LongDistance,
+    NightRail,
+    RegionalRail,
+    TouristRailway,
+
+    /**
+     * Metro sub modes
+     */
+    Metro,
+
+    /**
+     * Air sub modes
+     */
+    DomesticFlight,
+    HelicopterService,
+    InternationalFlight,
+
+    /**
+     * Water sub modes
+     */
+    HighSpeedPassengerService,
+    HighSpeedVehicleService,
+    InternationalCarFerry,
+    InternationalPassengerFerry,
+    LocalCarFerry,
+    LocalPassengerFerry,
+    NationalCarFerry,
+    SightseeingService,
+
+    /**
+     * Cabelway sub modes
+     */
+    Telecabin,
+
+    /**
+     * Funicular sub modes
+     */
+    Funicular
+
+}
