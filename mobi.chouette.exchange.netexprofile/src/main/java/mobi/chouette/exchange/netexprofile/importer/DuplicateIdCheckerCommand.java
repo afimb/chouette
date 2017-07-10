809c16ac1d105ad2537e8526fc3bdc615bdea268
Arne Seime <arne.seime@greenbird.com>
2017-07-10 12:44:16 +0200

    Duplicate id checker now reports in validation report instead of invisible action report

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/DuplicateIdCheckerCommand.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/DuplicateIdCheckerCommand.java
@@ -1,15 +1,19 @@
 package mobi.chouette.exchange.netexprofile.importer;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.naming.InitialContext;
+
 import mobi.chouette.common.Context;
 import mobi.chouette.common.chain.Command;
 import mobi.chouette.common.chain.CommandFactory;
 import mobi.chouette.exchange.netexprofile.importer.util.IdVersion;
-import mobi.chouette.exchange.report.ActionReporter;
-
-import javax.naming.InitialContext;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
+import mobi.chouette.exchange.netexprofile.importer.validation.AbstractNetexProfileValidator;
+import mobi.chouette.exchange.validation.report.DataLocation;
+import mobi.chouette.exchange.validation.report.ValidationReporter;
 
 public class DuplicateIdCheckerCommand implements Command{
 
@@ -17,20 +21,39 @@ public class DuplicateIdCheckerCommand implements Command{
 	public boolean execute(Context context) throws Exception {
 		boolean result = SUCCESS;
 		
-		ActionReporter actionReporter = ActionReporter.Factory.getInstance();
-   		Map<IdVersion, List<String>> commonIds = (Map<IdVersion, List<String>>) context.get(mobi.chouette.exchange.netexprofile.Constant.NETEX_COMMON_FILE_IDENTIFICATORS);
+		ValidationReporter validationReporter = ValidationReporter.Factory.getInstance();
 
+		@SuppressWarnings("unchecked")
+		Map<IdVersion, List<String>> commonIds = (Map<IdVersion, List<String>>) context.get(mobi.chouette.exchange.netexprofile.Constant.NETEX_COMMON_FILE_IDENTIFICATORS);
+
+		validationReporter.addItemToValidationReport(context, AbstractNetexProfileValidator._1_NETEX_DUPLICATE_IDS_ACROSS_COMMON_FILES, "E");
+		boolean noDuplicates = true;
 		
+		//
 		for(IdVersion id : commonIds.keySet()) {
 			List<String> filenameList = commonIds.get(id);
 			if(filenameList.size() > 1) {
-				for(String fileName : filenameList) {
-					// TODO better error code
-					actionReporter.addFileErrorInReport(context, fileName, ActionReporter.FILE_ERROR_CODE.INTERNAL_ERROR, "Duplicate id "+id);
+				
+				DataLocation sourceLocation = new DataLocation(filenameList.get(0), id.getLineNumber(), id.getColumnNumber());
+				
+				List<DataLocation> dataLocations = new ArrayList<>();
+				
+				for(int i=1; i<filenameList.size(); i++) {
+					DataLocation d = new DataLocation(filenameList.get(i));
+					dataLocations.add(d);
 				}
-				result = ERROR;
+				validationReporter.addCheckPointReportError(context, AbstractNetexProfileValidator._1_NETEX_DUPLICATE_IDS_ACROSS_COMMON_FILES,sourceLocation,id.getId(),null,dataLocations.toArray(new DataLocation[0]) );
+				noDuplicates = false;
 			}
 		}
+		
+		if(noDuplicates) {
+			validationReporter.reportSuccess(context, AbstractNetexProfileValidator._1_NETEX_DUPLICATE_IDS_ACROSS_COMMON_FILES);
+		} else {
+			result = ERROR;
+		}
+			
+		
 		return result;
 	}
 	
