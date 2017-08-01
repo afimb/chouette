45d7841ca7d73ecb1af257ddd4392f01248031b1
Arne Seime <arne.seime@greenbird.com>
2017-08-01 12:35:46 +0200

    Separate transfers.txt from stops.txt parsing (parse after lines). Fixed nullpointer in name generation

--- mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/importer/GtfsTransferParserCommand.java
+++ mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/importer/GtfsTransferParserCommand.java
@@ -4,13 +4,17 @@ import java.io.IOException;
 
 import javax.naming.InitialContext;
 
+import com.jamonapi.Monitor;
+import com.jamonapi.MonitorFactory;
+
 import lombok.extern.log4j.Log4j;
 import mobi.chouette.common.Color;
 import mobi.chouette.common.Context;
 import mobi.chouette.common.chain.Command;
 import mobi.chouette.common.chain.CommandFactory;
 import mobi.chouette.exchange.gtfs.Constant;
-import mobi.chouette.exchange.gtfs.parser.GtfsStopParser;
+import mobi.chouette.exchange.gtfs.model.importer.GtfsImporter;
+import mobi.chouette.exchange.gtfs.parser.GtfsTransferParser;
 import mobi.chouette.exchange.importer.ParserFactory;
 import mobi.chouette.exchange.report.ActionReporter;
 import mobi.chouette.exchange.report.ActionReporter.OBJECT_STATE;
@@ -18,13 +22,10 @@ import mobi.chouette.exchange.report.ActionReporter.OBJECT_TYPE;
 import mobi.chouette.exchange.report.IO_TYPE;
 import mobi.chouette.model.util.Referential;
 
-import com.jamonapi.Monitor;
-import com.jamonapi.MonitorFactory;
-
 @Log4j
-public class GtfsStopParserCommand implements Command, Constant {
+public class GtfsTransferParserCommand implements Command, Constant {
 
-	public static final String COMMAND = "GtfsStopParserCommand";
+	public static final String COMMAND = "GtfsTransferParserCommand";
 
 	@Override
 	public boolean execute(Context context) throws Exception {
@@ -39,16 +40,22 @@ public class GtfsStopParserCommand implements Command, Constant {
 				referential.clear(true);
 			}
 
-			// StopArea
-			if (referential.getSharedStopAreas().isEmpty()) {
-				GtfsStopParser gtfsStopParser = (GtfsStopParser) ParserFactory.create(GtfsStopParser.class.getName());
-				gtfsStopParser.parse(context);
+			GtfsImporter importer = (GtfsImporter) context.get(PARSER);
+
+			// ConnectionLink
+			if (importer.hasTransferImporter()) {
+				if (referential.getSharedConnectionLinks().isEmpty()) {
+					GtfsTransferParser gtfsTransferParser = (GtfsTransferParser) ParserFactory
+							.create(GtfsTransferParser.class.getName());
+					gtfsTransferParser.parse(context);
+				}
+			}
+			if (configuration.getMaxDistanceForConnectionLink() > 0) {
+				ConnectionLinkGenerator connectionLinkGenerator = new ConnectionLinkGenerator();
+				connectionLinkGenerator.createConnectionLinks(context);
+
 			}
 
-			if (configuration.getMaxDistanceForCommercial() > 0) {
-				CommercialStopGenerator commercialStopGenerator = new CommercialStopGenerator();
-				commercialStopGenerator.createCommercialStopPoints(context);
-			}
 
 			addStats(context, referential);
 
@@ -64,9 +71,10 @@ public class GtfsStopParserCommand implements Command, Constant {
 
 	private void addStats(Context context, Referential referential) {
 		ActionReporter reporter = ActionReporter.Factory.getInstance();
-		reporter.addObjectReport(context, "merged", OBJECT_TYPE.STOP_AREA, "stop areas", OBJECT_STATE.OK, IO_TYPE.INPUT);
-		reporter.setStatToObjectReport(context, "merged", OBJECT_TYPE.STOP_AREA, OBJECT_TYPE.STOP_AREA, referential
-				.getSharedStopAreas().size());
+		reporter.addObjectReport(context, "merged", OBJECT_TYPE.CONNECTION_LINK, "connection links", OBJECT_STATE.OK,
+				IO_TYPE.INPUT);
+		reporter.setStatToObjectReport(context, "merged", OBJECT_TYPE.CONNECTION_LINK, OBJECT_TYPE.CONNECTION_LINK,
+				referential.getSharedConnectionLinks().size());
 
 	}
 
@@ -74,12 +82,12 @@ public class GtfsStopParserCommand implements Command, Constant {
 
 		@Override
 		protected Command create(InitialContext context) throws IOException {
-			Command result = new GtfsStopParserCommand();
+			Command result = new GtfsTransferParserCommand();
 			return result;
 		}
 	}
 
 	static {
-		CommandFactory.factories.put(GtfsStopParserCommand.class.getName(), new DefaultCommandFactory());
+		CommandFactory.factories.put(GtfsTransferParserCommand.class.getName(), new DefaultCommandFactory());
 	}
 }
