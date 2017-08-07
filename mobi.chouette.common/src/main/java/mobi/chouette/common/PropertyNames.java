372462454807aa42535ebff77c2f0da3bd1f5917
erlendnils1 <erlendnils1@gmail.com>
2017-08-07 15:49:50 +0200

    NRP-1735 Removed obsolete props for updating external stop registry

--- mobi.chouette.common/src/main/java/mobi/chouette/common/PropertyNames.java
+++ mobi.chouette.common/src/main/java/mobi/chouette/common/PropertyNames.java
@@ -6,11 +6,5 @@ public interface PropertyNames {
     public static final String MAX_STARTED_JOBS = ".started.jobs.max";
     public static final String MAX_COPY_BY_JOB = ".copy.by.import.max";
 
-    /** Update stop place registry when importing */
-    public static final String STOP_PLACE_REGISTER_UPDATE = ".stop.place.register.update";
-
-    /** URL to the NeTEx endpoint accepting publication deliveries */
-    public static final String STOP_PLACE_REGISTER_URL = ".stop.place.register.url";
-
     public static final String RESCHEDULE_INTERRUPTED_JOBS = ".reschedule.interrupted.jobs";
 }
