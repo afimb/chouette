cc0a272ffc2449218efbd3a6784324b5c35ed353
Arne Seime <arne.seime@greenbird.com>
2017-08-10 08:45:46 +0200

    Made jaxbcontext and schema static to avoid slow reinitialization across imports

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/NetexImporter.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/NetexImporter.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashSet;
 import java.util.Set;
 
 import javax.xml.XMLConstants;
@@ -14,21 +13,17 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.namespace.QName;
-import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
-import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 
 import org.rutebanken.netex.model.PublicationDeliveryStructure;
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import lombok.extern.log4j.Log4j;
 import mobi.chouette.exchange.netexprofile.parser.xml.PredefinedSchemaListClasspathResourceResolver;
 import mobi.chouette.exchange.netexprofile.parser.xml.SkippingSAXSourceFactory;
-import net.sf.saxon.s9api.BuildingContentHandler;
 import net.sf.saxon.s9api.DocumentBuilder;
 import net.sf.saxon.s9api.Processor;
 import net.sf.saxon.s9api.SaxonApiException;
@@ -38,15 +33,16 @@ import net.sf.saxon.s9api.XdmNode;
 
 @Log4j
 public class NetexImporter {
-	private Schema netexSchema = null;
+	private static Schema netexSchema = null;
 
-	private JAXBContext netexJaxBContext = null;
+	private static JAXBContext netexJaxBContext = null;
 	
-	private Processor processor = new Processor(false);
+	private static Processor processor = new Processor(false);
 
 	public synchronized Schema getNetexSchema() throws SAXException, IOException {
 
 		if (netexSchema == null) {
+			log.info("Initializing Netex schema, this may take a few seconds");
 			SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 			factory.setResourceResolver(new PredefinedSchemaListClasspathResourceResolver("/netex_schema_list.txt"));
 
@@ -59,33 +55,12 @@ public class NetexImporter {
 
 	public JAXBContext getNetexJaxBContext() throws JAXBException {
 		if (netexJaxBContext == null) {
+			log.info("Initializing JAXBContext, this may take a few seconds");
 			netexJaxBContext = JAXBContext.newInstance("net.opengis.gml._3:org.rutebanken.netex.model:uk.org.siri.siri");
 		}
 
 		return netexJaxBContext;
 	}
-
-//	public Document parseFileToDom(File f) throws SAXException, IOException, ParserConfigurationException {
-//		return parseFileToDom(f, new HashSet<>());
-//	}
-//
-//	public Document parseFileToDom(File f, Set<QName> elementsToSkip) throws SAXException, IOException, ParserConfigurationException {
-//		FileInputStream fis = new FileInputStream(f);
-//		BufferedInputStream bis = new BufferedInputStream(fis);
-//
-//		Document doc = PositionalXMLReader.readXML(bis, elementsToSkip);
-//		bis.close();
-//
-//		return doc;
-//
-//		// DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//		// factory.setNamespaceAware(true);
-//		// DocumentBuilder builder = factory.newDocumentBuilder();
-//		//
-//		// Document document = builder.parse(f);
-//		//
-//		// return document;
-//	}
 
 	@SuppressWarnings("unchecked")
 	public PublicationDeliveryStructure unmarshal(File file) throws JAXBException {
@@ -96,17 +71,6 @@ public class NetexImporter {
 		return commonDeliveryStructure.getValue();
 	}
 
-//	@SuppressWarnings("unchecked")
-//	public PublicationDeliveryStructure unmarshal(Document d) throws JAXBException {
-//		JAXBContext netexJaxBContext = getNetexJaxBContext();
-//		Unmarshaller createUnmarshaller = netexJaxBContext.createUnmarshaller();
-//		// LocationListener locationListener = new LocationListener();
-//		// createUnmarshaller.setListener(locationListener);
-//		JAXBElement<PublicationDeliveryStructure> commonDeliveryStructure = (JAXBElement<PublicationDeliveryStructure>) createUnmarshaller
-//				.unmarshal(new DOMSource(d));
-//		return commonDeliveryStructure.getValue();
-//	}
-//
 	public XdmNode parseFileToXdmNode(File file, Set<QName> elementsToSkip) throws SaxonApiException, FileNotFoundException, IOException, SAXException {
         
         DocumentBuilder builder = processor.newDocumentBuilder();
