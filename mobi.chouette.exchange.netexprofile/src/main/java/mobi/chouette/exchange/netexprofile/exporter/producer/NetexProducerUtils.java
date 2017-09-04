97ec22cd0d90edbea2db709ed53d7b37a6721d3f
Arne Seime <arne@seimeconsulting.com>
2017-09-04 15:59:33 +0200

    Cleanup

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/NetexProducerUtils.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/producer/NetexProducerUtils.java
@@ -4,32 +4,17 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.rutebanken.netex.model.DayOfWeekEnumeration;
 import org.rutebanken.netex.model.OrganisationTypeEnumeration;
 
 import lombok.extern.log4j.Log4j;
-import mobi.chouette.model.AccessLink;
-import mobi.chouette.model.AccessPoint;
-import mobi.chouette.model.Company;
-import mobi.chouette.model.ConnectionLink;
-import mobi.chouette.model.GroupOfLine;
-import mobi.chouette.model.JourneyPattern;
-import mobi.chouette.model.Line;
 import mobi.chouette.model.NeptuneIdentifiedObject;
-import mobi.chouette.model.Network;
-import mobi.chouette.model.Route;
-import mobi.chouette.model.StopArea;
-import mobi.chouette.model.StopPoint;
-import mobi.chouette.model.Timetable;
-import mobi.chouette.model.VehicleJourney;
 import mobi.chouette.model.type.DayTypeEnum;
 import mobi.chouette.model.type.OrganisationTypeEnum;
 
@@ -130,43 +115,6 @@ public class NetexProducerUtils {
         return dayOfWeekEnumerations;
     }
 
-    private static String netexModelName(NeptuneIdentifiedObject model) {
-        if (model == null)
-            return null;
-        if (model instanceof StopArea) {
-            return "StopArea";
-        } else if (model instanceof AccessPoint) {
-            return "AccessPoint";
-        } else if (model instanceof Company) {
-            return "Operator";
-        } else if (model instanceof AccessLink) {
-            return "AccessLink";
-        } else if (model instanceof StopPoint) {
-            return "StopPoint";
-        } else if (model instanceof Network) {
-            return "GroupOfLine";
-        } else if (model instanceof Line) {
-            return "Line";
-        } else if (model instanceof Route) {
-            return "Route";
-        } else if (model instanceof GroupOfLine) {
-            return "GroupOfLine";
-        } else if (model instanceof JourneyPattern) {
-            return "JourneyPattern";
-        } else if (model instanceof ConnectionLink) {
-            return "ConnectionLink";
-        } else if (model instanceof Timetable) {
-            return "Timetable";
-        } else if (model instanceof VehicleJourney) {
-            return "ServiceJourney";
-        } else {
-            return null;
-        }
-    }
-
-    public static final int DEFAULT_START_INCLUSIVE = 1111111;
-    public static final int DEFAULT_END_EXCLUSIVE = 8888888;
-    
     private static AtomicInteger idCounter = new AtomicInteger(1);
 
     public static int generateSequentialId() {
@@ -174,12 +122,18 @@ public class NetexProducerUtils {
     }
 
   
-    public static String netexId(NeptuneIdentifiedObject model) {
-        return model == null ? null : model.objectIdPrefix() + OBJECT_ID_SPLIT_CHAR + netexModelName(model) + OBJECT_ID_SPLIT_CHAR + model.objectIdSuffix();
-    }
-
     public static String netexId(String objectIdPrefix, String elementName, String objectIdSuffix) {
         return objectIdPrefix + OBJECT_ID_SPLIT_CHAR + elementName + OBJECT_ID_SPLIT_CHAR + objectIdSuffix;
+    }
+    
+    
+    
+    public static String translateObjectId(String original, String newType) {
+    	return original.replaceAll(original, newType);
+    }
+    
+    public static String netexId(NeptuneIdentifiedObject chouetteObject, String newType) {
+    	return translateObjectId(chouetteObject.getObjectId(), newType);
     }
 
     public static String objectIdPrefix(String objectId) {
