8488fe1a3f5a7bd849812f91d1536ee00b8ab45f
Arne Seime <arne@seimeconsulting.com>
2017-09-19 23:05:11 +0200

    Do not skip lines on export without network or operatorref

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/NetexDataCollector.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/exporter/NetexDataCollector.java
@@ -1,10 +1,10 @@
 package mobi.chouette.exchange.netexprofile.exporter;
 
+import org.joda.time.LocalDate;
+
 import lombok.extern.log4j.Log4j;
 import mobi.chouette.exchange.exporter.DataCollector;
 import mobi.chouette.model.Line;
-
-import org.joda.time.LocalDate;
 
 @Log4j
 public class NetexDataCollector extends DataCollector {
@@ -12,19 +12,6 @@ public class NetexDataCollector extends DataCollector {
     public boolean collect(ExportableData collection, Line line, LocalDate startDate, LocalDate endDate) {
         boolean res =  collect(collection, line, startDate, endDate, false, false);
 
-        
-        
-        if (line.getNetwork() == null) {
-            log.error("line " + line.getObjectId() + " : missing network");
-            return false;
-        }
-
-        if (line.getCompany() == null) {
-            log.error("line " + line.getObjectId() + " : missing company");
-            return false;
-        }
-
-        
         if(line.getNetwork().getCompany() != null) {
         	collection.getCompanies().add(line.getNetwork().getCompany());
         }
