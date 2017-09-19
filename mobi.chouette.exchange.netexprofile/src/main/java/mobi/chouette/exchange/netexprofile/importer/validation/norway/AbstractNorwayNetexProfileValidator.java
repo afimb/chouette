2dab0157a76206d6c4a2a595ca8c419e486d224d
Arne Seime <arne@seimeconsulting.com>
2017-09-19 09:58:37 +0200

    Export using 1.07:NO-NeTEx-networktimetable:1.1

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/validation/norway/AbstractNorwayNetexProfileValidator.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/validation/norway/AbstractNorwayNetexProfileValidator.java
@@ -24,9 +24,10 @@ import net.sf.saxon.s9api.XdmNode;
 
 public abstract class AbstractNorwayNetexProfileValidator extends AbstractNetexProfileValidator {
 
-	public static final String PROFILE_ID_1 = "1.04:NO-NeTEx-networktimetable:1.0";
-	public static final String PROFILE_ID_1_1 = "1.04:NO-NeTEx-networktimetable:1.1";
-	public static final String EXPORT_PROFILE_ID = PROFILE_ID_1_1; // Update when new profile version is implemented
+	public static final String PROFILE_NORWAY_NETWORKTIMETABLE_104_10 = "1.04:NO-NeTEx-networktimetable:1.0";
+	public static final String PROFILE_NORWAY_NETWORKTIMETABLE_104_11 = "1.04:NO-NeTEx-networktimetable:1.1";
+	public static final String PROFILE_NORWAY_NETWORKTIMETABLE_107_11 = "1.07:NO-NeTEx-networktimetable:1.1";
+	public static final String EXPORT_PROFILE_ID = PROFILE_NORWAY_NETWORKTIMETABLE_107_11; // Update when new profile version is implemented
 	
 	public static final String NSR_XMLNSURL = "http://www.rutebanken.org/ns/nsr";
 	public static final String NSR_XMLNS = "NSR";
@@ -276,7 +277,7 @@ public abstract class AbstractNorwayNetexProfileValidator extends AbstractNetexP
 	}
 
 	public Collection<String> getSupportedProfiles() {
-		return Arrays.asList(new String[] { PROFILE_ID_1,PROFILE_ID_1_1 });
+		return Arrays.asList(new String[] { PROFILE_NORWAY_NETWORKTIMETABLE_104_10,PROFILE_NORWAY_NETWORKTIMETABLE_104_11, PROFILE_NORWAY_NETWORKTIMETABLE_107_11 });
 	}
 
 	protected void verifyReferencesToCorrectEntityTypes(Context context, List<IdVersion> localRefs) {
