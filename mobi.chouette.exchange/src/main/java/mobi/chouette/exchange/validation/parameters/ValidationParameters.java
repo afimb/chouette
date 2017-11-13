8f5847daebb9723eabb088ed5afe4a2b62651c71
erlendnils1 <erlendnils1@gmail.com>
2017-11-13 14:53:43 +0100

    Fixed duplicate additions for modeFunicular

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/validation/parameters/ValidationParameters.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/validation/parameters/ValidationParameters.java
@@ -162,15 +162,6 @@ public class ValidationParameters {
 
 	@XmlElement(name = "mode_tram")
 	private TransportModeParameters modeTram;
-	
-	@XmlElement(name = "mode_funicular")
-	private TransportModeParameters modeFunicular;
-
-	@XmlElement(name = "mode_rail")
-	private TransportModeParameters modeRail;
-
-	@XmlElement(name = "mode_funicular")
-	private TransportModeParameters modeFunicular;
 
 	@XmlElement(name = "mode_funicular")
 	private TransportModeParameters modeFunicular;
