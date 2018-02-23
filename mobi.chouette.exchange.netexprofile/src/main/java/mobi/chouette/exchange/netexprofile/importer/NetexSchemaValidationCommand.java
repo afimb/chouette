c0b32ea8f7f675d1c924f3f4f279794cf366865b
erlendnils1 <erlendnils1@gmail.com>
2018-02-23 15:09:26 +0100

    Reverted change limiting no of executors to one less than available processors. Should not be needed.

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/NetexSchemaValidationCommand.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/importer/NetexSchemaValidationCommand.java
@@ -72,9 +72,8 @@ public class NetexSchemaValidationCommand implements Command, Constant {
 				return t;
 			}
 		};
-		int nThreads = Math.max(1, Runtime.getRuntime().availableProcessors() - 1);
-		log.info("Starting executorService for validation with threads: " + nThreads);
-		ExecutorService executor = Executors.newFixedThreadPool(nThreads,threadFactory);
+		int processors = Runtime.getRuntime().availableProcessors();
+		ExecutorService executor = Executors.newFixedThreadPool(processors,threadFactory);;
 
 		try {
 			List<Future<SchemaValidationTask>> schemaValidationResults = new ArrayList<>();
