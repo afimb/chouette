e6042c6b7d0873b80ad3b033ee5391399a0a8dac
erlendnils1 <erlendnils1@gmail.com>
2018-02-27 14:13:28 +0100

    ROR-206 Created separate checkpoints for the details of vehicleJourney_2 that should be error severity (multiple severities for same checkpoint not supported).

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/validation/checkpoint/VehicleJourneyCheckPoints.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/validation/checkpoint/VehicleJourneyCheckPoints.java
@@ -78,8 +78,8 @@ public class VehicleJourneyCheckPoints extends AbstractValidation<VehicleJourney
 
 		initCheckPoint(context, VEHICLE_JOURNEY_1, SEVERITY.W);
 		initCheckPoint(context, VEHICLE_JOURNEY_2, SEVERITY.I);
-		initCheckPoint(context, VEHICLE_JOURNEY_2,"1", SEVERITY.E);
-		initCheckPoint(context, VEHICLE_JOURNEY_2,"5", SEVERITY.E);
+		initCheckPoint(context, VEHICLE_JOURNEY_RB_1, SEVERITY.E);
+		initCheckPoint(context, VEHICLE_JOURNEY_RB_2,SEVERITY.E);
 		initCheckPoint(context, VEHICLE_JOURNEY_3, SEVERITY.W);
 		initCheckPoint(context, VEHICLE_JOURNEY_4, SEVERITY.W);
 		initCheckPoint(context, VEHICLE_JOURNEY_5, SEVERITY.E);
@@ -92,8 +92,8 @@ public class VehicleJourneyCheckPoints extends AbstractValidation<VehicleJourney
 		// checkPoint is applicable
 		prepareCheckPoint(context, VEHICLE_JOURNEY_1);
 		prepareCheckPoint(context, VEHICLE_JOURNEY_2);
-		prepareCheckPoint(context, VEHICLE_JOURNEY_2,"1");
-		prepareCheckPoint(context, VEHICLE_JOURNEY_2,"5");
+		prepareCheckPoint(context, VEHICLE_JOURNEY_RB_1);
+		prepareCheckPoint(context, VEHICLE_JOURNEY_RB_2);
 		prepareCheckPoint(context, VEHICLE_JOURNEY_4);
 		prepareCheckPoint(context, VEHICLE_JOURNEY_5);
 		prepareCheckPoint(context, VEHICLE_JOURNEY_5,"1");
@@ -282,7 +282,7 @@ public class VehicleJourneyCheckPoints extends AbstractValidation<VehicleJourney
 					DataLocation target2 = buildLocation(context, vjas1.getStopPoint().getScheduledStopPoint().getContainedInStopAreaRef().getObject());
 	
 					ValidationReporter reporter = ValidationReporter.Factory.getInstance();
-					reporter.addCheckPointReportError(context, VEHICLE_JOURNEY_2, "1", source, null, null, target1, target2);
+					reporter.addCheckPointReportError(context, VEHICLE_JOURNEY_RB_1, null, source, null, null, target1, target2);
 				} else {
 					
 					double distance = getDistance(vjas0.getStopPoint().getScheduledStopPoint().getContainedInStopAreaRef().getObject(), vjas1.getStopPoint().getScheduledStopPoint()
@@ -303,17 +303,21 @@ public class VehicleJourneyCheckPoints extends AbstractValidation<VehicleJourney
 							reporter.addCheckPointReportError(context, VEHICLE_JOURNEY_2, "2", source,
 	                                calculatedSpeed, Integer.toString((int) minSpeed), target1, target2);
 						} else if (speed > maxSpeed) {
-							// Set detail = 5 (ERROR) for violations with more than twice the allowed speed
-							String detail = speed > 2 * maxSpeed ? "5" : "3";
 
+							double hardLimit = maxSpeed * 2;
 							// trop rapide
 							DataLocation source = buildLocation(context, vj);
 							DataLocation target1 = buildLocation(context, vjas0.getStopPoint().getScheduledStopPoint().getContainedInStopAreaRef().getObject());
 							DataLocation target2 = buildLocation(context, vjas1.getStopPoint().getScheduledStopPoint().getContainedInStopAreaRef().getObject());
-	
 							ValidationReporter reporter = ValidationReporter.Factory.getInstance();
-							reporter.addCheckPointReportError(context, VEHICLE_JOURNEY_2, detail, source,
-	                                calculatedSpeed, Integer.toString((int) maxSpeed), target1, target2);
+							if (speed > hardLimit) {
+								reporter.addCheckPointReportError(context, VEHICLE_JOURNEY_RB_2, null, source,
+										calculatedSpeed, Integer.toString((int) hardLimit), target1, target2);
+							} else {
+								reporter.addCheckPointReportError(context, VEHICLE_JOURNEY_2, "3", source,
+										calculatedSpeed, Integer.toString((int) maxSpeed), target1, target2);
+							}
+
 						}
 					}
 				}
