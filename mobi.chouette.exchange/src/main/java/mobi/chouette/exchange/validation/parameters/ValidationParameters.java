decd096a81fdc21c90da5d405408092991c7c0fe
erlendnils1 <erlendnils1@gmail.com>
2018-02-28 10:50:03 +0100

    Added new param to propOrder

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/validation/parameters/ValidationParameters.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/validation/parameters/ValidationParameters.java
@@ -16,6 +16,7 @@ import lombok.Data;
 		"interStopAreaDistanceMin",
 		"parentStopAreaDistanceMax",
 		"interAccessPointDistanceMin",
+		"maxSpeedHardLimitFactor",
 		"interConnectionLinkDistanceMax",
 		"walkDefaultSpeedMax",
 		"walkOccasionalTravellerSpeedMax",
