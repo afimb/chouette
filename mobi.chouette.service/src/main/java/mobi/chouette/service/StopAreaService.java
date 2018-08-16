983780c64897496b5c83175fa2654583d757641f
erlendnils1 <erlendnils1@gmail.com>
2018-08-16 14:40:19 +0200

    Switched to parallel execution of stop area reference updates per referential

--- mobi.chouette.service/src/main/java/mobi/chouette/service/StopAreaService.java
+++ mobi.chouette.service/src/main/java/mobi/chouette/service/StopAreaService.java
@@ -1,8 +1,17 @@
 package mobi.chouette.service;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -11,6 +20,7 @@ import lombok.extern.log4j.Log4j;
 import mobi.chouette.common.Constant;
 import mobi.chouette.common.Context;
 import mobi.chouette.dao.ReferentialDAO;
+import mobi.chouette.exchange.netexprofile.importer.NetexSchemaValidationCommand;
 import mobi.chouette.exchange.report.ActionReport;
 import mobi.chouette.exchange.stopplace.PublicationDeliveryStopPlaceParser;
 import mobi.chouette.exchange.stopplace.StopAreaUpdateContext;
@@ -23,68 +33,109 @@ import mobi.chouette.persistence.hibernate.ContextHolder;
 @Log4j
 public class StopAreaService {
 
-    public static final String BEAN_NAME = "StopAreaService";
+	public static final String BEAN_NAME = "StopAreaService";
 
-    @EJB(beanName = StopAreaUpdateService.BEAN_NAME)
-    private StopAreaUpdateService stopAreaUpdateService;
+	@EJB(beanName = StopAreaUpdateService.BEAN_NAME)
+	private StopAreaUpdateService stopAreaUpdateService;
 
-    @EJB
-    private ReferentialDAO referentialDAO;
+	@EJB
+	private ReferentialDAO referentialDAO;
+
+	private ExecutorService executor;
+
+	public StopAreaService() {
+		final AtomicInteger counter = new AtomicInteger(0);
+		ThreadFactory threadFactory = (r) -> {
+			Thread t = new Thread(r);
+			t.setName("stop-area-reference-updater-thread-" + (counter.incrementAndGet()));
+			t.setPriority(Thread.MIN_PRIORITY);
+			return t;
+		};
+		int processors = Runtime.getRuntime().availableProcessors();
+		executor = Executors.newFixedThreadPool(processors, threadFactory);
+		;
+
+	}
+
+	public void createOrUpdateStopPlacesFromNetexStopPlaces(InputStream inputStream) {
+		PublicationDeliveryStopPlaceParser parser = new PublicationDeliveryStopPlaceParser(inputStream);
+
+		StopAreaUpdateContext updateContext = parser.getUpdateContext();
+		int changedStopCnt = updateContext.getChangedStopCount();
+
+		if (changedStopCnt > 0) {
+			log.info("Updating " + changedStopCnt + " stop areas");
+			Context context = createContext();
+			ContextHolder.clear();
+			stopAreaUpdateService.createOrUpdateStopAreas(context, updateContext);
+			log.info("Updated " + changedStopCnt + " stop areas");
+			updateStopAreaReferencesPerReferential(updateContext.getMergedQuays());
+		} else {
+			log.debug("Received update without any stop areas. Doing nothing");
+		}
+	}
+
+	private void updateStopAreaReferencesPerReferential(Map<String, Set<String>> replacementMap) {
+		int updatedStopPointCnt = 0;
+		List<Future<Integer>> futures = new ArrayList();
+
+		for (String referential : referentialDAO.getReferentials()) {
+			StopAreaUpdateTask updateTask = new StopAreaUpdateTask(referential, replacementMap);
+			futures.add(executor.submit(updateTask));
+		}
+		try {
+			for (Future<Integer> future : futures) {
+				updatedStopPointCnt += future.get();
+			}
+		} catch (ExecutionException e) {
+			throw new RuntimeException("Exception while updating StopArea references: " + e.getMessage(), e);
+		} catch (InterruptedException ie) {
+			throw new RuntimeException("Interrupted while waiting for StopArea reference update", ie);
+		}
 
 
-    public void createOrUpdateStopPlacesFromNetexStopPlaces(InputStream inputStream) {
-        PublicationDeliveryStopPlaceParser parser = new PublicationDeliveryStopPlaceParser(inputStream);
+		log.info("Updated stop area references for " + updatedStopPointCnt + " stop points");
+	}
 
-        StopAreaUpdateContext updateContext=parser.getUpdateContext();
-        int changedStopCnt = updateContext.getChangedStopCount();
+	public void deleteStopArea(String objectId) {
+		ContextHolder.clear();
+		stopAreaUpdateService.deleteStopArea(objectId);
+	}
 
-        if (changedStopCnt > 0) {
-            log.info("Updating " + changedStopCnt + " stop areas");
-            Context context = createContext();
-            ContextHolder.clear();
-            stopAreaUpdateService.createOrUpdateStopAreas(context, updateContext);
-            log.info("Updated " + changedStopCnt + " stop areas");
-            updateStopAreaReferencesPerReferential(updateContext.getMergedQuays());
-        } else {
-            log.debug("Received update without any stop areas. Doing nothing");
-        }
-    }
-
-    private void updateStopAreaReferencesPerReferential(Map<String, Set<String>> replacementMap) {
-        int updatedStopPointCnt = 0;
-        String orgContext = ContextHolder.getContext();
-        try {
-            for (String referential : referentialDAO.getReferentials()) {
-                ContextHolder.setContext(referential);
-                log.info("Updating stop area references for " + updatedStopPointCnt + " stop points for referential " + referential);
-                updatedStopPointCnt += stopAreaUpdateService.updateStopAreaReferences(replacementMap);
-                log.info("Updated stop area references for " + updatedStopPointCnt + " stop points for referential " + referential);
-            }
-        } finally {
-            ContextHolder.setContext(orgContext); // reset context
-        }
-        log.info("Updated stop area references for " + updatedStopPointCnt + " stop points");
-    }
-
-    public void deleteStopArea(String objectId) {
-        ContextHolder.clear();
-        stopAreaUpdateService.deleteStopArea(objectId);
-    }
-
-    public void deleteUnusedStopAreas() {
-        ContextHolder.clear();
-        stopAreaUpdateService.deleteUnusedStopAreas();
-    }
+	public void deleteUnusedStopAreas() {
+		ContextHolder.clear();
+		stopAreaUpdateService.deleteUnusedStopAreas();
+	}
 
 
-    private Context createContext() {
-        Context context = new Context();
-        Referential referential = new Referential();
-        context.put(Constant.REFERENTIAL, referential);
-        context.put(Constant.CACHE, referential);
-        context.put(Constant.REPORT, new ActionReport());
-        context.put(Constant.VALIDATION_REPORT, new ValidationReport());
-        return context;
-    }
+	private Context createContext() {
+		Context context = new Context();
+		Referential referential = new Referential();
+		context.put(Constant.REFERENTIAL, referential);
+		context.put(Constant.CACHE, referential);
+		context.put(Constant.REPORT, new ActionReport());
+		context.put(Constant.VALIDATION_REPORT, new ValidationReport());
+		return context;
+	}
 
+
+	class StopAreaUpdateTask implements Callable<Integer> {
+
+		private final String referential;
+		private final Map<String, Set<String>> replacementMap;
+
+		public StopAreaUpdateTask(String referential, Map<String, Set<String>> replacementMap) {
+			this.referential = referential;
+			this.replacementMap = replacementMap;
+		}
+
+		@Override
+		public Integer call() throws Exception {
+			ContextHolder.setContext(referential);
+			log.debug("Updating stop area references for stop points for referential " + referential);
+			int updatedCnt = stopAreaUpdateService.updateStopAreaReferences(replacementMap);
+			log.debug("Updated stop area references for " + updatedCnt + " stop points for referential " + referential);
+			return updatedCnt;
+		}
+	}
 }
