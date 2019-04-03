a4ad44d80f20b14f6548906c497478b02baa003d
erlendnils1 <erlendnils1@gmail.com>
2019-04-03 18:29:48 +0200

    Avoid nullpointer for null fullProfileString

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/model/NetexProfileVersion.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/model/NetexProfileVersion.java
@@ -1,14 +1,16 @@
 package mobi.chouette.exchange.netexprofile.model;
 
 public class NetexProfileVersion {
-	
+
 	//1.04:NO-NeTEx-networktimetable:1.0
 	public static String getSchemaVersion(String fullProfileString) {
-		String[] split = fullProfileString.split(":");
-	
-		if(split.length == 3) {
-			// Valid
-			return split[0];
+		if (fullProfileString != null) {
+			String[] split = fullProfileString.split(":");
+
+			if (split.length == 3) {
+				// Valid
+				return split[0];
+			}
 		}
 		return null;
 	}
