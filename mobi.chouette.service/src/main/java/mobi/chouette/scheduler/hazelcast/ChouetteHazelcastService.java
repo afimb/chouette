a55102771e54bbd4daeb2c08127a9415714e4622
erlendnils1 <erlendnils1@gmail.com>
2019-04-03 20:18:49 +0200

    Made time to live for hazelcast locks configurable

--- mobi.chouette.service/src/main/java/mobi/chouette/scheduler/hazelcast/ChouetteHazelcastService.java
+++ mobi.chouette.service/src/main/java/mobi/chouette/scheduler/hazelcast/ChouetteHazelcastService.java
@@ -6,8 +6,6 @@ import lombok.extern.log4j.Log4j;
 
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.IMap;
-import com.hazelcast.core.MemberAttributeEvent;
-import com.hazelcast.core.MembershipEvent;
 import com.hazelcast.core.MembershipListener;
 import org.rutebanken.hazelcasthelper.service.HazelCastService;
 import org.rutebanken.hazelcasthelper.service.KubernetesService;
@@ -19,8 +17,9 @@ public class ChouetteHazelcastService extends HazelCastService {
 
 	private static final String MAP_CONFIG_NAME_JOB_LOCKS = "JobLocks";
 
+	private static final String HAZELCAST_TTL_SECONDS_KEY = "iev.hazelcast.ttl.seconds";
 
-	private static final int TTL_SECONDS = 10800;
+	private static final int DEFAULT_TTL_SECONDS = 10800;
 
 	public ChouetteHazelcastService(KubernetesService kubernetesService, List<MembershipListener> listeners) {
 		super(kubernetesService);
@@ -42,21 +41,23 @@ public class ChouetteHazelcastService extends HazelCastService {
 	public List<MapConfig> getAdditionalMapConfigurations() {
 		List<MapConfig> mapConfigs = super.getAdditionalMapConfigurations();
 
+		int ttlSeconds = getTtlSeconds();
+
 		mapConfigs.add(
 				new MapConfig()
 						.setName(MAP_CONFIG_NAME_REFERENTIAL_LOCKS)
 						.setBackupCount(1)
 						.setAsyncBackupCount(2)
-						.setTimeToLiveSeconds(TTL_SECONDS));
+						.setTimeToLiveSeconds(ttlSeconds));
 
 		mapConfigs.add(
 				new MapConfig()
 						.setName(MAP_CONFIG_NAME_JOB_LOCKS)
 						.setBackupCount(1)
 						.setAsyncBackupCount(2)
-						.setTimeToLiveSeconds(TTL_SECONDS));
+						.setTimeToLiveSeconds(ttlSeconds));
 
-		log.info("Configured map for referential locks:  " + mapConfigs.get(0));
+		log.info("Configured map for referential locks:  " + mapConfigs.get(0) + " with time to live = " + ttlSeconds);
 		return mapConfigs;
 	}
 
@@ -67,6 +68,14 @@ public class ChouetteHazelcastService extends HazelCastService {
 
 	private void addMembershipListeners(List<MembershipListener> listeners) {
 		listeners.forEach(listener -> hazelcast.getCluster().addMembershipListener(listener));
+	}
+
+	private int getTtlSeconds() {
+		String ttlAsString = System.getProperty(HAZELCAST_TTL_SECONDS_KEY);
+		if (ttlAsString != null) {
+			return Integer.parseInt(ttlAsString);
+		}
+		return DEFAULT_TTL_SECONDS;
 	}
 
 
